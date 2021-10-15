ef86d9962fc1f094b8488ad81878cd8cf0801e26
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2021-10-15 13:32:08 +0000

    Sync LeetCode submission - Valid Palindrome (java)

---
+++ leetcode-solutions/problems/valid_palindrome/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i=0, j=s.length()-1;
+        s=s.toLowerCase();
+        while(i<j){
+            char ci=s.charAt(i);
+            char cj=s.charAt(j);
+            if(!Character.isLetterOrDigit(ci)){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(cj)){
+                j--;
+                continue;
+            }
+            
+            if(ci!=cj)
+                return false; 
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
