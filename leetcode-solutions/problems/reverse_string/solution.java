47a6b0083ee759feb5f8f4e432e54286f445c28c
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2023-04-22 18:59:10 +0000

    Sync LeetCode submission - Reverse String (java)

--- leetcode-solutions/problems/reverse_string/solution.java
+++ leetcode-solutions/problems/reverse_string/solution.java
@@ -1,11 +1,9 @@
 class Solution {
     public void reverseString(char[] s) {
-        int len=s.length;
-        for(int i=0;i<len/2;i++){
-            char temp=s[i];
-            s[i]=s[len-i-1];
-            s[len-i-1]=temp;
+        for(int i=0,j=s.length-1;i<j;i++,j--){
+            char temp = s[i];
+            s[i]=s[j];
+            s[j]=temp;
         }
-    
     }
 }
