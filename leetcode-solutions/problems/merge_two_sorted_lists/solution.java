9548413eafba0e2f92e5779894b86baf86a675ce
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2024-07-11 02:32:32 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (java)

--- leetcode-solutions/problems/merge_two_sorted_lists/solution.java
+++ leetcode-solutions/problems/merge_two_sorted_lists/solution.java
@@ -9,49 +9,43 @@
  * }
  */
 class Solution {
-//     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    // public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+    //     if(list1==null) return list2;
         
-//         if(list1==null) return list2;
-        
-//         if(list2==null) return list1;
-        
-//         if(list1.val<=list2.val){
-//             list1.next=mergeTwoLists(list1.next,list2);
-//             return list1;
-//         }
-        
-//         else{
-//             list2.next=mergeTwoLists(list1,list2.next);
-//             return list2;
-//         }
-//     }
-    
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
-        ListNode dummy=new ListNode(0);
-        ListNode l=dummy;
-        while(l1!=null && l2!=null){
-            
-            
-            if(l1.val<=l2.val){
-                l.next=new ListNode(l1.val);
-                l1=l1.next;
+    //     if(list2==null) return list1;
+
+    //     if(list1.val<=list2.val) {
+    //         list1.next = mergeTwoLists(list1.next,list2);
+    //         return list1;
+    //     }
+
+    //     else {
+    //         list2.next = mergeTwoLists(list1,list2.next);
+    //         return list2;
+    //     }
+    // }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode list = new ListNode(0);
+        ListNode start = list;
+        while(list1!=null && list2!=null) {
+            if(list1.val <= list2.val) {
+                list.next=new ListNode(list1.val);
+                list1=list1.next;
             }
-            else{
-                l.next=new ListNode(l2.val);
-                l2=l2.next;
+            else {
+                list.next=new ListNode(list2.val);
+                list2=list2.next;
             }
-            
-            l=l.next;
+            list=list.next;
         }
-        
-        if(l1==null)
-            l.next=l2;
-            
-        if(l2==null)
-            l.next=l1;
-        
-        return dummy.next;
-        
+
+        if(list2==null) {
+            list.next=list1;
+        }
+        else {
+            list.next = list2;
+        }
+        return start.next;
     }
 }
