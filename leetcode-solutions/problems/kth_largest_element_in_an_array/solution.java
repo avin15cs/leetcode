7e59368972f9921f08fd73fd9490f6d7b8c1b40f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-08-26 03:36:09 +0000

    Sync LeetCode submission - Kth Largest Element in an Array (java)

--- leetcode-solutions/problems/kth_largest_element_in_an_array/solution.java
+++ leetcode-solutions/problems/kth_largest_element_in_an_array/solution.java
@@ -1,49 +1,12 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        int n=nums.length;
-        int i=0,j=n-1;
-
-        int pos=n-k;
-
-        while(i<=j){
-            int pivot=partition(nums, i, j);
-
-            if(pivot< pos){
-                i=pivot+1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i: nums){
+            pq.add(i);
+            if(pq.size()>k){
+                pq.remove();
             }
-
-            else if(pivot>pos){
-                j=pivot-1;
-            }
-            else
-                return nums[pivot];
         }
-
-        return -1;
-
+        return pq.peek();
     }
-
-    private int partition(int[] nums, int l, int h){
-        int i=l,j=h;
-        int pivot=nums[l];
-
-        while(i<j){
-            while(i<=h && nums[i]<=pivot)   i++;
-
-            while(j>=l && nums[j]>pivot)    j--;
-
-            if(i<j) swap(nums,i,j);
-        }
-
-        swap(nums,j,l);
-
-        return j;
-    }
-
-    private void swap(int[] nums, int i, int j){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
-    }
-
 }
