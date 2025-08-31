a4c3dc57a6255940b6689cf9eb1c12d448c6abba
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-08-31 09:06:22 +0000

    Sync LeetCode submission - Jump Game (java)

---
+++ leetcode-solutions/problems/jump_game/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        // int maxSteps=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(maxSteps<i) return false;
+        //     maxSteps=Math.max(maxSteps,i+nums[i]);
+        // }
+        // return true;
+        
+        Boolean[] dp=new Boolean[nums.length+1];
+        return findPath(0,nums,dp);
+    }
+
+    private boolean findPath(int currentPos, int[] nums, Boolean[] dp) {
+        if(currentPos == nums.length-1) return dp[currentPos]=true;
+
+        if(nums[currentPos]==0) return dp[currentPos] = false;
+
+        if(dp[currentPos]!=null) return dp[currentPos];
+
+        for(int i=currentPos+1;i<=currentPos+nums[currentPos];i++){
+            if(findPath(i,nums,dp))
+                return dp[currentPos]=true;
+        }
+        return dp[currentPos]=false;
+    }
+}
