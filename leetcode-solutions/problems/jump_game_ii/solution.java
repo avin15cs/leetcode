4c4213068c115607a09e95ace6e96e134a9e180a
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-09-20 11:43:01 +0000

    Sync LeetCode submission - Jump Game II (java)

--- leetcode-solutions/problems/jump_game_ii/solution.java
+++ leetcode-solutions/problems/jump_game_ii/solution.java
@@ -1,16 +1,33 @@
 class Solution {
+    // public int jump(int[] nums) {
+    //     int l=0,r=0,jumps=0,n=nums.length;
+    //     while(r<n-1) {
+    //         int farthest=0;
+    //         for(int i = l;i<=r;i++) {
+    //             farthest = Math.max(farthest,i+nums[i]);
+    //         }
+    //         l=r+1;
+    //         r=farthest;
+    //         jumps++;
+    //     }
+
+    //     return jumps;
+    // }
+
     public int jump(int[] nums) {
-        int l=0,r=0,jumps=0,n=nums.length;
-        while(r<n-1) {
-            int farthest=0;
-            for(int i = l;i<=r;i++) {
-                farthest = Math.max(farthest,i+nums[i]);
+        int curEnd=0, jump=0, farthest = 0;
+        int n=nums.length;
+        for(int i=0;i<nums.length-1;i++){
+            farthest=Math.max(farthest, i+nums[i]);
+
+            if(i==curEnd){
+                jump++;
+                curEnd=farthest;
             }
-            l=r+1;
-            r=farthest;
-            jumps++;
+
+            if(curEnd==n-1) break;
         }
 
-        return jumps;
+        return jump;
     }
 }
