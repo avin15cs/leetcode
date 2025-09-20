f4bfed6d5a40c5ecb87f426b9b4295d478c6923f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-09-20 17:16:57 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

---
+++ leetcode-solutions/problems/search_in_rotated_sorted_array/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        int l=0,j=n-1;
+        if(nums.length==0) return -1;
+        if(nums.length==1) return target==nums[0]?0:-1;
+        if(nums[0]<nums[j]) return binarySearch(nums,0,j,target);
+
+        int idx=findIndexOfSmallestNo(nums);
+
+        return Math.max(binarySearch(nums,0,idx-1,target),binarySearch(nums,idx,j,target));
+    }
+
+    int findIndexOfSmallestNo(int[] nums) {
+        int n=nums.length;
+        int l=0,h=n-1;
+        int ans=-1;
+        while(l<=h) {
+            int mid=l+(h-l)/2;
+            if(nums[0]>nums[mid]){
+                ans=mid;
+                h=mid-1;
+            }
+            else
+                l=mid+1;
+        }
+
+        return ans;
+        
+    }
+
+
+    int binarySearch(int[] nums, int i, int j, int target){
+
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(nums[mid]==target) return mid;
+            else if(nums[mid]>target) j=mid-1;
+            else i=mid+1;
+        }
+        return -1;
+    }
+}
