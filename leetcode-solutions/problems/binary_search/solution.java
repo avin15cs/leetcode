52373d88577b3e2a2ecffba60625e690b1424e5f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-09-27 17:12:45 +0000

    Sync LeetCode submission - Binary Search (java)

--- leetcode-solutions/problems/binary_search/solution.java
+++ leetcode-solutions/problems/binary_search/solution.java
@@ -1,21 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        int i=0, j=nums.length-1;
-        
-        while(i<=j){
-            int mid=i+(j-i)/2;
-            
+        int l=0,r=nums.length-1;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
             if(nums[mid]==target)
                 return mid;
-            
+
             else if(nums[mid]<target)
-                i=mid+1;
+                l=mid+1;
             
             else
-                j=mid-1;
+                r=mid-1;
         }
-        
+
         return -1;
     }
 }
