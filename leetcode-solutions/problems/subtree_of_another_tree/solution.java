758875c5e2fe61a95a46aa2c69dae94b2f3a8708
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-09-28 17:14:50 +0000

    Sync LeetCode submission - Subtree of Another Tree (java)

--- leetcode-solutions/problems/subtree_of_another_tree/solution.java
+++ leetcode-solutions/problems/subtree_of_another_tree/solution.java
@@ -15,20 +15,24 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root==null)  return root==subRoot;
-        
-        if(check(root,subRoot)) return true;
+        if(root==null||subRoot==null) return root==subRoot;
+
+        boolean chek = false;
+        if(root.val==subRoot.val) {
+            chek=checkSubtree(root,subRoot);
+        }
+            
+        if(chek) return true;
         
         return isSubtree(root.left,subRoot)||isSubtree(root.right,subRoot);
     }
-    
-    private boolean check(TreeNode root, TreeNode subRoot){
-        if(root==null)  return root==subRoot;
-        
-        if(subRoot==null)   return false;
-        
+
+
+    boolean checkSubtree(TreeNode root, TreeNode subRoot) {
+        if(root==null||subRoot==null) return root==subRoot;
+
         if(root.val!=subRoot.val)   return false;
-        
-        return check(root.left,subRoot.left) && check(root.right,subRoot.right);
+
+        return checkSubtree(root.left,subRoot.left) && checkSubtree(root.right,subRoot.right);
     }
 }
