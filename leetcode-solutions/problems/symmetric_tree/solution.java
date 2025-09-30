0722e61c253e5f2092958efde6397f7ef26c3d20
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-09-30 15:51:04 +0000

    Sync LeetCode submission - Symmetric Tree (java)

--- leetcode-solutions/problems/symmetric_tree/solution.java
+++ leetcode-solutions/problems/symmetric_tree/solution.java
@@ -15,18 +15,18 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        
         if(root==null)  return true;
-        
-        return checkSymmetry(root.left,root.right);
+
+        return check(root.left, root.right);
+
     }
-    
-    private boolean checkSymmetry(TreeNode left, TreeNode right){
-        
-        if(left==null || right== null)  return left==right;
-        
+
+    boolean check(TreeNode left, TreeNode right) {
+
+        if(left==null||right==null) return left==right;
+
         if(left.val!=right.val) return false;
-        
-        return checkSymmetry(left.left,right.right) && checkSymmetry(left.right,right.left);
+
+        return check(left.left, right.right) && check(left.right,right.left);
     }
 }
