f03b807aae316df27c91ac88ad6ec03a678d5228
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-10-09 18:32:50 +0000

    Sync LeetCode submission - Jump Game (java)

--- leetcode-solutions/problems/jump_game/solution.java
+++ leetcode-solutions/problems/jump_game/solution.java
@@ -1,5 +1,5 @@
 class Solution {
-    public boolean canJump(int[] nums) {
+    // public boolean canJump(int[] nums) {
         // int maxSteps=0;
         // for(int i=0;i<nums.length;i++){
         //     if(maxSteps<i) return false;
@@ -7,21 +7,35 @@ class Solution {
         // }
         // return true;
         
-        Boolean[] dp=new Boolean[nums.length+1];
-        return findPath(0,nums,dp);
-    }
+    //     Boolean[] dp=new Boolean[nums.length+1];
+    //     return findPath(0,nums,dp);
+    // }
 
-    private boolean findPath(int currentPos, int[] nums, Boolean[] dp) {
-        if(currentPos == nums.length-1) return dp[currentPos]=true;
+    // private boolean findPath(int currentPos, int[] nums, Boolean[] dp) {
+    //     if(currentPos == nums.length-1) return dp[currentPos]=true;
 
-        if(nums[currentPos]==0) return dp[currentPos] = false;
+    //     if(nums[currentPos]==0) return dp[currentPos] = false;
 
-        if(dp[currentPos]!=null) return dp[currentPos];
+    //     if(dp[currentPos]!=null) return dp[currentPos];
 
-        for(int i=currentPos+1;i<=currentPos+nums[currentPos];i++){
-            if(findPath(i,nums,dp))
-                return dp[currentPos]=true;
+    //     for(int i=currentPos+1;i<=currentPos+nums[currentPos];i++){
+    //         if(findPath(i,nums,dp))
+    //             return dp[currentPos]=true;
+    //     }
+    //     return dp[currentPos]=false;
+    // }
+
+
+
+    public boolean canJump(int[] nums)
+    {
+        int pos=0;
+        for(int i=0;i<nums.length;i++) {
+            if(pos<i)
+                return false;
+            pos=Math.max(pos,nums[i]+i);
         }
-        return dp[currentPos]=false;
+
+        return true;
     }
 }
