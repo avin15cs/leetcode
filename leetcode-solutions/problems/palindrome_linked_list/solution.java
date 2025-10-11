08e77739c9012e0585f9587af58c64529fef2c80
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-10-11 11:21:08 +0000

    Sync LeetCode submission - Palindrome Linked List (java)

--- leetcode-solutions/problems/palindrome_linked_list/solution.java
+++ leetcode-solutions/problems/palindrome_linked_list/solution.java
@@ -10,43 +10,31 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        
-        ListNode mid= middle(head);
-        ListNode rev=reverse(mid);
-        
-        while(rev!=null){
-            if(head.val!=rev.val)
+        ListNode fast=head,slow=head;
+
+        while(fast!=null && fast.next!=null) {
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+
+        ListNode mid=slow;
+        ListNode prev=null;
+
+        while(mid!=null) {
+            ListNode temp=mid.next;
+            mid.next=prev;
+
+            prev=mid;
+            mid=temp;
+        }
+
+        while(prev!=null && head!=null) {
+            if(prev.val!=head.val)
                 return false;
+            
+            prev=prev.next;
             head=head.next;
-            rev=rev.next;
         }
         return true;
     }
-    public ListNode middle(ListNode head) {
-        ListNode slow=head;
-        ListNode fast=head;
-        
-        while(fast!=null && fast.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
-        }
-        
-        return slow;
-    }
-     public ListNode reverse(ListNode mid) {
-         
-         ListNode cur=mid;
-         ListNode prev=null,next=null;
-         
-         while(cur!=null){
-             next=cur.next;
-             cur.next=prev;
-             
-             prev=cur;
-             cur=next;
-         }
-         
-         return prev;
-         
-     }
 }
