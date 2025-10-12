0702168c43b33d365f1981246e152a034cfb9990
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-10-12 16:35:56 +0000

    Sync LeetCode submission - Reverse Bits (java)

--- leetcode-solutions/problems/reverse_bits/solution.java
+++ leetcode-solutions/problems/reverse_bits/solution.java
@@ -1,16 +1,16 @@
-public class Solution {
-    // you need treat n as an unsigned value
+class Solution {
     public int reverseBits(int n) {
-        int rev=0,mask;
-        for(int i=0;i<32;i++) {
-            mask=n&1;
+        int rev=0;
+        for(int i=1;i<=32;i++){
+            int mask=n&1;
             rev=rev<<1;
+
             if(mask==1)
                 rev=rev|1;
             
-
             n=n>>1;
         }
+
         return rev;
     }
 }
