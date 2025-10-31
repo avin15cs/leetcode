cd04f5036acb3ef99f100ac4bbab212546ba4afc
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-10-31 16:50:30 +0000

    Sync LeetCode submission - Remove Element (java)

--- leetcode-solutions/problems/remove_element/solution.java
+++ leetcode-solutions/problems/remove_element/solution.java
@@ -1,12 +1,13 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int j=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]!=val)
-                nums[j++]=nums[i];
+        int k=0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]!=val) {
+                nums[k++]=nums[i];
+            }
         }
 
-        return j;
+        return k;
 
     }
 }
