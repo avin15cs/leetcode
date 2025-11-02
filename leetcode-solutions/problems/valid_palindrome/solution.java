2a8d23b0eefbd4874dc56ceb6b3c468acf6be5c1
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-11-02 03:24:44 +0000

    Sync LeetCode submission - Valid Palindrome (java)

--- leetcode-solutions/problems/valid_palindrome/solution.java
+++ leetcode-solutions/problems/valid_palindrome/solution.java
@@ -1,19 +1,39 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String str = s.toLowerCase();
+        // String str = s.toLowerCase();
 
-        int i=0,j=str.length()-1;
+        // int i=0,j=str.length()-1;
+
+        // while(i<j) {
+        //     while(i<j && !Character.isLetterOrDigit(str.charAt(i))){
+        //         i++;
+        //     }
+
+        //     while(i<j && !Character.isLetterOrDigit(str.charAt(j))){
+        //         j--;
+        //     }
+
+        //     if(str.charAt(i)!=str.charAt(j))
+        //         return false;
+            
+        //     i++;
+        //     j--;
+        // }
+
+        // return true;
+
+        String st=s.toLowerCase();
+
+        int i=0,j=st.length()-1;
 
         while(i<j) {
-            while(i<j && !Character.isLetterOrDigit(str.charAt(i))){
+            while(i<j && !Character.isLetterOrDigit(st.charAt(i)))
                 i++;
-            }
 
-            while(i<j && !Character.isLetterOrDigit(str.charAt(j))){
+            while(i<j && !Character.isLetterOrDigit(st.charAt(j)))
                 j--;
-            }
-
-            if(str.charAt(i)!=str.charAt(j))
+            
+            if(st.charAt(i)!=st.charAt(j))
                 return false;
             
             i++;
