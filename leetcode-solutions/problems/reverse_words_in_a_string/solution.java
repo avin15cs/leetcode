91e2a1de8ce8b877a7fcb7f073f6dee5fb53ba81
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-11-08 08:45:39 +0000

    Sync LeetCode submission - Reverse Words in a String (java)

--- leetcode-solutions/problems/reverse_words_in_a_string/solution.java
+++ leetcode-solutions/problems/reverse_words_in_a_string/solution.java
@@ -1,41 +1,41 @@
 class Solution {
-    // public String reverseWords(String s) {
-    //     s=s.trim();
-    //     String[] words= s.split("\\s+");
-    //     int i=0,j=words.length-1;
-    //     while(i<j){
-    //         String temp=words[i];
-    //         words[i]=words[j];
-    //         words[j]=temp;
-    //         i++;
-    //         j--;
-    //     }
-    //     return String.join(" ",words);
-
-    // }
     public String reverseWords(String s) {
-        StringBuilder str = new StringBuilder(s);
-        str.reverse(); //reverse entire String
-        int n= s.length();
-        
-        String ans="";
-
-        for(int i=0;i<n;i++){
-            StringBuilder word = new StringBuilder();
-
-            //Extract word
-            while(i<n && str.charAt(i)!=' '){
-                word.append(str.charAt(i));
-                i++;
-            }
-
-            //step 3: Reverse word and append if not empty
-            if(word.length()>0) {
-                word.reverse();
-                ans +=" " + word.toString();
-            }
-
+        s=s.trim();
+        String[] words= s.split("\\s+");
+        int i=0,j=words.length-1;
+        while(i<j){
+            String temp=words[i];
+            words[i]=words[j];
+            words[j]=temp;
+            i++;
+            j--;
         }
-        return ans.trim();
+        return String.join(" ",words);
+
     }
+    // public String reverseWords(String s) {
+    //     StringBuilder str = new StringBuilder(s);
+    //     str.reverse(); //reverse entire String
+    //     int n= s.length();
+        
+    //     String ans="";
+
+    //     for(int i=0;i<n;i++){
+    //         StringBuilder word = new StringBuilder();
+
+    //         //Extract word
+    //         while(i<n && str.charAt(i)!=' '){
+    //             word.append(str.charAt(i));
+    //             i++;
+    //         }
+
+    //         //step 3: Reverse word and append if not empty
+    //         if(word.length()>0) {
+    //             word.reverse();
+    //             ans +=" " + word.toString();
+    //         }
+
+    //     }
+    //     return ans.trim();
+    // }
 }
