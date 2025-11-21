b34d80c612f41fe4e828b27929e5bb24b26031f7
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-11-21 12:13:41 +0000

    Sync LeetCode submission - Find Median from Data Stream (java)

---
+++ leetcode-solutions/problems/find_median_from_data_stream/solution.java
@@ -0,0 +1,50 @@
+// class MedianFinder {
+//     PriorityQueue<Integer> left;
+//     PriorityQueue<Integer> right;
+//     public MedianFinder() {
+//         left=new PriorityQueue<>(Collections.reverseOrder());
+//         right=new PriorityQueue<>();
+//     }
+    
+//     public void addNum(int num) {
+//         left.add(num);
+//         right.add(num);
+//         if(left.size()<right.size())
+//             left.add(right.poll());
+//     }
+    
+//     public double findMedian() {
+//         if(left.size()==right.size())
+//             return (left.peek()+right.peek())/2.0;
+//         return left.peek();
+//     }
+// }
+
+class MedianFinder {
+    PriorityQueue<Integer> left;
+    PriorityQueue<Integer> right;
+    public MedianFinder() {
+        left=new PriorityQueue<>(Collections.reverseOrder());
+        right=new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        left.add(num);
+        right.add(left.poll());
+        if(left.size()<right.size())
+            left.add(right.poll());
+    }
+    
+    public double findMedian() {
+        if(left.size()==right.size())
+            return (left.peek()+right.peek())/2.0;
+        return left.peek();
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
