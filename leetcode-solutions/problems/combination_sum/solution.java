0bb353d528c407861f75c09f0aec37b33f53eff4
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-07 15:34:05 +0000

    Sync LeetCode submission - Combination Sum (java)

--- leetcode-solutions/problems/combination_sum/solution.java
+++ leetcode-solutions/problems/combination_sum/solution.java
@@ -6,24 +6,22 @@ class Solution {
         return res;
     }
 
-    public void solve(int[] candidates, int target, int n, List<List<Integer>> res, List<Integer> ans)  {
-        if(target==0) {
-            List<Integer> list = new ArrayList<>(ans);
+    void solve(int[] nums, int sum, int n, List<List<Integer>> res, List<Integer> ans) {
+        if(sum==0) {
+            List<Integer> list=new ArrayList<>(ans);
             res.add(list);
             return;
         }
-        if(n<=0||target<=0) {
+
+        if(n<=0||sum<=0) {
             return;
         }
 
-        if(candidates[n-1]<=target) {
-            solve(candidates,target,n-1,res,ans);
-            ans.add(candidates[n-1]);
-            solve(candidates,target-candidates[n-1],n,res,ans);
+        if(nums[n-1]<=sum) {
+            ans.add(nums[n-1]);
+            solve(nums,sum-nums[n-1],n,res,ans);
             ans.remove(ans.size()-1);
-        } else {
-            solve(candidates,target,n-1,res,ans);
         }
-
+        solve(nums,sum,n-1,res,ans);
     }
 }
