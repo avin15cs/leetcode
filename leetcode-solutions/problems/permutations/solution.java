52ef215fa5cf3d30eb8e71f42cd1e751b1f97ada
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-07 15:05:39 +0000

    Sync LeetCode submission - Permutations (java)

--- leetcode-solutions/problems/permutations/solution.java
+++ leetcode-solutions/problems/permutations/solution.java
@@ -1,19 +1,21 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        solve(nums,new LinkedHashSet<>(),ans);
-        return ans;
+        List<List<Integer>> list=new ArrayList<>();
+        solve(nums,new LinkedHashSet<>(),list);
+        return list;
     }
-    public void solve(int[] nums, Set<Integer> path, List<List<Integer>> res) {
-        if(path.size()==nums.length) {
-            res.add(new ArrayList<>(path));
+
+    private void solve(int[] nums,Set<Integer> set,List<List<Integer>> list) {
+        if(set.size()==nums.length) {
+            List<Integer> ans=new ArrayList<>(set);
+            list.add(ans);
             return;
         }
 
-        for(int num: nums) {
-            if(path.add(num)){
-                solve(nums,path,res);
-                path.remove(num);
+        for(int i=0;i<nums.length;i++) {
+            if(set.add(nums[i])) {
+                solve(nums,set,list);
+                set.remove(nums[i]);
             }
         }
     }
