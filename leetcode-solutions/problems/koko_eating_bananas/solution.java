83f6e928b331c05dc014d48a00a04add7527c3f9
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-07 02:25:44 +0000

    Sync LeetCode submission - Koko Eating Bananas (java)

--- leetcode-solutions/problems/koko_eating_bananas/solution.java
+++ leetcode-solutions/problems/koko_eating_bananas/solution.java
@@ -1,29 +1,32 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int n) {
         int h=0;
-        for(int i: piles) {
-            h=Math.max(i,h);
-        }
+        for(int pile: piles)
+            h=Math.max(pile,h);
+        
         int l=1;
+
         while(l<=h) {
             int mid=l+(h-l)/2;
-            long minSum=checkMin(piles,mid);
-            if(minSum<=n) {
+            long reqHours=requiredHours(piles,mid);
+            if(reqHours<=n) {
                 h=mid-1;
             } else {
                 l=mid+1;
             }
         }
+
         return l;
     }
 
-    private long checkMin(int[] piles, int speed) {
+    long requiredHours(int[] piles, int speed) {
         long hours=0;
-        for(int pile: piles) {
-            hours+=pile/speed;
+        for(int pile:piles) {
+            hours+=(pile/speed);
             if(pile%speed>0)
-                hours++;   
+                hours++;
         }
+
         return hours;
     }
 }
