2705e394c8214ef87b44817c83a0c7f82cf4bb01
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-10 01:36:22 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- leetcode-solutions/problems/longest_increasing_subsequence/solution.java
+++ leetcode-solutions/problems/longest_increasing_subsequence/solution.java
@@ -20,35 +20,40 @@ class Solution {
     //         return dp[n][prev+1]=Math.max(take,skip);
     //     } else {
     //         int skip=solve(nums,n+1,prev,dp);
-    //         return dp[n][prev+1]=skip;
+    //         return dp[n][prev+1]=skip;   
     //     }
     // }
     public int lengthOfLIS(int[] nums) {
+        int n=nums.length;
+
         List<Integer> list=new ArrayList<>();
-        list.add(nums[0]);
         int prev=nums[0];
-        for(int i=1;i<nums.length;i++) {
+        list.add(nums[0]);
+
+        for(int i=1;i<n;i++) {
             if(nums[i]>list.get(list.size()-1)) {
                 list.add(nums[i]);
                 prev=nums[i];
             } else {
-                int pos=findPos(list,0,list.size()-1,nums[i]);
+                int pos=findPos(list,nums[i]);
                 list.set(pos,nums[i]);
             }
         }
-
         return list.size();
     }
 
-    public int findPos(List<Integer> list, int i, int j, int target) {
-        while(i<=j) {
-            int mid=i+(j-i)/2;
+    private int findPos(List<Integer> list, int target) {
+        int n=list.size();
 
-            if(list.get(mid)<target) {
+        int i=0, j=n-1;
+
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(list.get(mid)<target) 
                 i=mid+1;
-            } else {
+            
+            else
                 j=mid-1;
-            }
         }
 
         return i;
