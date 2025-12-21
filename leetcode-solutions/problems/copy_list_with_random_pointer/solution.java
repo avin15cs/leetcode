b4501ccb03dac33de099761972d367e3675fcea9
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-21 17:18:36 +0000

    Sync LeetCode submission - Copy List with Random Pointer (java)

--- leetcode-solutions/problems/copy_list_with_random_pointer/solution.java
+++ leetcode-solutions/problems/copy_list_with_random_pointer/solution.java
@@ -15,43 +15,24 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        // Map<Integer,Node> map=new HashMap<>();
-        // Node newN=new Node(0);
-        // Node cur=newN;
-        // Node node=head;
-        // while(head!=null) {
-        //     cur.next=new Node(head.val);
-        //     cur=cur.next;
-        //     head=head.next;
-        //     map.put(cur.val,cur);
-        // }
-        // cur=newN.next;
-        // while(node!=null){
-        //     if(node.random!=null)
-        //         cur.random=map.get(node.random.val);
-        //     cur=cur.next;
-        //     node=node.next;
-        // }
-
-        // return newN.next;
-
         Node cur=head;
         Map<Node,Node> map=new HashMap<>();
 
-        while(cur!=null){
-            Node newNode=new Node(cur.val);
-            map.put(cur,newNode);
+        while(cur!=null) {
+            Node node = new Node(cur.val);
+            map.put(cur,node);
             cur=cur.next;
         }
 
         cur=head;
-        while(cur!=null){
+        while(cur!=null) {
             Node node=map.get(cur);
             Node next=map.get(cur.next);
             Node rand=map.get(cur.random);
 
             node.next=next;
             node.random=rand;
+
             cur=cur.next;
         }
 
