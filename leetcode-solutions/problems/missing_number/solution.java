e90adf5709ffe13b3d07731d32282250983efe7a
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-22 04:34:08 +0000

    Sync LeetCode submission - Missing Number (java)

--- leetcode-solutions/problems/missing_number/solution.java
+++ leetcode-solutions/problems/missing_number/solution.java
@@ -1,14 +1,36 @@
 class Solution {
+    // public int missingNumber(int[] nums) {
+    //     int n=nums.length;
+    //     int i=0;
+    //     while(i<n) {
+    //         int correct=nums[i];
+    //         if(correct<n && nums[i]!=nums[correct]) {
+    //             int temp=nums[i];
+    //             nums[i]=nums[correct];
+    //             nums[correct]=temp;
+    //         }
+    //         else
+    //             i++;
+    //     }
+
+    //     for(i=0;i<nums.length;i++) {
+    //         if(i!=nums[i])
+    //             return i;
+    //     }
+
+    //     return nums.length;
+    // }
+
     public int missingNumber(int[] nums) {
-        int sum=nums.length*(nums.length+1)/2;
-        int num=nums.length;
-        for(int i=0;i<nums.length;i++)
-        {
-            sum-=nums[i];
-            // num ^= i;
-            // num ^= nums[i];;
+        
+        int n=nums.length;
+        int num=n;
+
+        for(int i=0;i<n;i++) {
+            num=num^i;
+            num=num^nums[i];
         }
-        return sum;
-        // return num;
+
+        return num;
     }
 }
