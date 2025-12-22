253da38c6e89286db0efc1150a2c0879d4699878
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-22 02:50:50 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- leetcode-solutions/problems/valid_anagram/solution.java
+++ leetcode-solutions/problems/valid_anagram/solution.java
@@ -15,26 +15,42 @@ class Solution {
     //     return sCount.equals(tCount);        
     // }
 
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
+
+    //     int[] ch=new int[26];
+    //     for(int i=0;i<s.length();i++) {
+    //         ch[s.charAt(i)-'a']++;
+    //         ch[t.charAt(i)-'a']--;
+    //     }
+
+    //     for(int i=0;i<26;i++) {
+    //         if(ch[i]!=0)
+    //             return false;
+    //     }
+
+    //     return true;
+
+    // }
+
+
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
 
-        int[] ch=new int[26];
-        for(int i=0;i<s.length();i++) {
-            char sc = s.charAt(i);
-            char tc = t.charAt(i);
+        char sc[]=s.toCharArray();
+        char tc[]=t.toCharArray();
+        Arrays.sort(sc);
+        Arrays.sort(tc);
 
-            ch[sc-'a']++;
-            ch[tc-'a']--;
-        }
+        int i=0;
+        int n=sc.length;
+        while(i<n && sc[i]==tc[i])
+            i++;
 
-        for(int i=0;i<26;i++) {
-            if(ch[i]!=0)
-                return false;
-        }
-
-        return true;
-
+        return i==n;
     }
 }
