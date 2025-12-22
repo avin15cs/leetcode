82709b6af22c468d18e29ba4aeaf51a697aad7f6
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-22 05:01:54 +0000

    Sync LeetCode submission - Subsets (java)

--- leetcode-solutions/problems/subsets/solution.java
+++ leetcode-solutions/problems/subsets/solution.java
@@ -1,8 +1,8 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        int n=nums.length-1;
-        return solve(nums,new ArrayList<>(),n);
-    }
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     int n=nums.length-1;
+    //     return solve(nums,new ArrayList<>(),n);
+    // }
 
     // List<List<Integer>> solve(int[] nums, List<Integer> cur, int n) {
     //     if(n<0) {
@@ -12,30 +12,33 @@ class Solution {
     //     }
     //     List<List<Integer>> left = solve(nums,cur,n-1);
 
-    //     List<Integer> newList = new ArrayList<>(cur);
-    //     newList.add(nums[n]);
+    //     cur.add(nums[n]);
 
-    //     List<List<Integer>> right=solve(nums,newList,n-1);
+    //     List<List<Integer>> right=solve(nums,cur,n-1);
     //     left.addAll(right);
+        
+    //     cur.remove(cur.size()-1);
 
     //     return left;
     // }
 
-    List<List<Integer>> solve(int[] nums, List<Integer> cur, int n) {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans=new ArrayList<>();
+        solve(nums,nums.length-1,ans, new ArrayList<>());
+        return ans;
+    }
+
+
+    private void solve(int[] nums, int n, List<List<Integer>> ans, List<Integer> list) {
         if(n<0) {
-            List<List<Integer>> ans=new ArrayList<>();
-            ans.add(new ArrayList<>(cur));
-            return ans;
+            List<Integer> cur=new ArrayList<>(list);
+            ans.add(cur);
+            return;
         }
-        List<List<Integer>> left = solve(nums,cur,n-1);
 
-        cur.add(nums[n]);
-
-        List<List<Integer>> right=solve(nums,cur,n-1);
-        left.addAll(right);
-        
-        cur.remove(cur.size()-1);
-
-        return left;
+        solve(nums, n-1, ans, list);
+        list.add(nums[n]);
+        solve(nums, n-1, ans, list);
+        list.remove(list.size()-1);
     }
 }
