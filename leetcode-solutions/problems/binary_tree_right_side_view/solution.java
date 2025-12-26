828bf6b26b85187c736ada8e91e032c0f032e08f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-26 12:22:45 +0000

    Sync LeetCode submission - Binary Tree Right Side View (java)

--- leetcode-solutions/problems/binary_tree_right_side_view/solution.java
+++ leetcode-solutions/problems/binary_tree_right_side_view/solution.java
@@ -14,24 +14,51 @@
  * }
  */
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
 
-        Queue<TreeNode> que=new LinkedList<>();
+    //BFS approach
+    // public List<Integer> rightSideView(TreeNode root) {
+
+    //     Queue<TreeNode> que=new LinkedList<>();
+    //     List<Integer> list=new ArrayList<>();
+    //     if(root==null) return list;
+        
+    //     que.add(root);
+
+    //     while(!que.isEmpty()) {
+    //         int size=que.size();
+
+    //         for(int i=0;i<size;i++) {
+    //             TreeNode node=que.remove();
+                
+    //             if(i==size-1)
+    //                 list.add(node.val);
+
+    //             if(node.left!=null)
+    //                 que.add(node.left);
+
+    //             if(node.right!=null)
+    //                 que.add(node.right);
+    //         }
+    //     }
+
+    //     return list;
+    // }
+
+    //DFS Approach
+    public List<Integer> rightSideView(TreeNode root) {
         List<Integer> list=new ArrayList<>();
-        if(root==null) return list;
-        que.add(root);
-        while(!que.isEmpty()){
-            int size=que.size();
-            for(int i=0;i<size;i++){
-                TreeNode node=que.remove();
-                if(i==size-1)
-                    list.add(node.val);
-                if(node.left!=null)
-                    que.add(node.left);
-                if(node.right!=null)
-                    que.add(node.right);
-            }
-        }
+        solve(root,0,list);
         return list;
+    }
+
+    void solve(TreeNode root, int depth, List<Integer> list) {
+        if(root==null)
+            return;
+
+        if(list.size()==depth)
+            list.add(root.val);
+        
+        solve(root.right,depth+1,list);
+        solve(root.left,depth+1,list);
     }
 }
