69ba924995f487670c9eb75f45494ee065279990
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-26 08:00:09 +0000

    Sync LeetCode submission - Two Sum II - Input Array Is Sorted (java)

--- leetcode-solutions/problems/two_sum_ii_-_input_array_is_sorted/solution.java
+++ leetcode-solutions/problems/two_sum_ii_-_input_array_is_sorted/solution.java
@@ -1,20 +1,20 @@
 class Solution {
 
     //Regular way
-    // public int[] twoSum(int[] nums, int target) {
-    //     int i=0,j=nums.length-1;
+    public int[] twoSum(int[] nums, int target) {
+        int i=0,j=nums.length-1;
 
-    //     while(i<j){
-    //         int sum=nums[i]+nums[j];
-    //         if(sum<target) i++;
+        while(i<j){
+            int sum=nums[i]+nums[j];
+            if(sum<target) i++;
 
-    //         else if(sum>target) j--;
+            else if(sum>target) j--;
 
-    //         else return new int[]{i+1,j+1};
-    //     }
+            else return new int[]{i+1,j+1};
+        }
 
-    //     return new int[]{-1,-1};
-    // }
+        return new int[]{-1,-1};
+    }
 
     //Using HashMap
     // public int[] twoSum(int[] numbers, int target) {
@@ -34,25 +34,25 @@ class Solution {
     // }
 
     //Using Binary Search
-    public int[] twoSum(int[] numbers, int target) {
-        for (int i = 0; i < numbers.length; i++) {
-            int left = i + 1;
-            int right = numbers.length - 1;
-            int tmp = target - numbers[i];
+    // public int[] twoSum(int[] numbers, int target) {
+    //     for (int i = 0; i < numbers.length; i++) {
+    //         int left = i + 1;
+    //         int right = numbers.length - 1;
+    //         int tmp = target - numbers[i];
             
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
+    //         while (left <= right) {
+    //             int mid = left + (right - left) / 2;
                 
-                if (numbers[mid] == tmp) {
-                    return new int[]{i + 1, mid + 1};
-                } else if (numbers[mid] < tmp) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
-                }
-            }
-        }
+    //             if (numbers[mid] == tmp) {
+    //                 return new int[]{i + 1, mid + 1};
+    //             } else if (numbers[mid] < tmp) {
+    //                 left = mid + 1;
+    //             } else {
+    //                 right = mid - 1;
+    //             }
+    //         }
+    //     }
         
-        return new int[]{-1, -1};
-    }
+    //     return new int[]{-1, -1};
+    // }
 }
