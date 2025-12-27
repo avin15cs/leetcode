be62e924b37dd0f3ea5c27ce1e510b2954284e8e
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-27 04:32:53 +0000

    Sync LeetCode submission - Reorder List (java)

---
+++ leetcode-solutions/problems/reorder_list/solution.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!=null) {
+            fast=fast.next.next;
+            slow=slow.next;
+        }
+
+        ListNode second = reverse(slow.next);
+
+        slow.next = null;
+
+        ListNode first = head;
+
+        while(second!=null) {
+            ListNode firstNext = first.next;
+            ListNode secondNext = second.next;
+
+            first.next=second;
+            second.next=firstNext;
+
+            first=firstNext;
+            second=secondNext;
+            
+        }
+    }
+
+
+    ListNode reverse(ListNode node) {
+        ListNode prev = null;
+
+        while(node!=null) {
+            ListNode temp=node.next;
+            node.next = prev;
+
+            prev = node;
+            node = temp;
+        }
+
+        return prev;
+    }
+}
