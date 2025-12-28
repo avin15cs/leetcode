cda908c27872fa2c4b08b9a7fb8885ac2f773a1c
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2025-12-28 04:11:20 +0000

    Sync LeetCode submission - Combination Sum (java)

--- leetcode-solutions/problems/combination_sum/solution.java
+++ leetcode-solutions/problems/combination_sum/solution.java
@@ -1,27 +1,49 @@
 class Solution {
-    public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> res=new ArrayList<>();
-        solve(candidates,target,candidates.length, res, new ArrayList<>());
+    // public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    //     List<List<Integer>> res=new ArrayList<>();
+    //     solve(candidates,target,candidates.length, res, new ArrayList<>());
 
+    //     return res;
+    // }
+
+    // void solve(int[] nums, int sum, int n, List<List<Integer>> res, List<Integer> ans) {
+    //     if(sum==0) {
+    //         List<Integer> list=new ArrayList<>(ans);
+    //         res.add(list);
+    //         return;
+    //     }
+
+    //     if(n<=0||sum<=0) {
+    //         return;
+    //     }
+
+    //     if(nums[n-1]<=sum) {
+    //         ans.add(nums[n-1]);
+    //         solve(nums,sum-nums[n-1],n,res,ans);
+    //         ans.remove(ans.size()-1);
+    //     }
+    //     solve(nums,sum,n-1,res,ans);
+    // }
+
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(candidates, 0, target, new ArrayList<>(), res);
         return res;
     }
 
-    void solve(int[] nums, int sum, int n, List<List<Integer>> res, List<Integer> ans) {
-        if(sum==0) {
-            List<Integer> list=new ArrayList<>(ans);
-            res.add(list);
+    private void backtrack(int[] arr, int start, int target, List<Integer> curr, List<List<Integer>> res) {
+        if(target == 0) {
+            res.add(new ArrayList<>(curr));
             return;
         }
+        
+        if(target < 0) return;
 
-        if(n<=0||sum<=0) {
-            return;
+        for(int i = start; i < arr.length; i++) {
+            if (arr[i] > target) continue;
+            curr.add(arr[i]);  // include arr[i]
+            backtrack(arr, i, target - arr[i], curr, res); // i not i+1 → reuse allowed
+            curr.remove(curr.size() - 1); // backtrack
         }
-
-        if(nums[n-1]<=sum) {
-            ans.add(nums[n-1]);
-            solve(nums,sum-nums[n-1],n,res,ans);
-            ans.remove(ans.size()-1);
-        }
-        solve(nums,sum,n-1,res,ans);
     }
 }
