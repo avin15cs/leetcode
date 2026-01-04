ba78c5e9b4b840db4d4877b9ef51101af88844fb
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-04 08:13:05 +0000

    Sync LeetCode submission - Sort Colors (java)

--- leetcode-solutions/problems/sort_colors/solution.java
+++ leetcode-solutions/problems/sort_colors/solution.java
@@ -1,31 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        
-        int l=0;
-        int m=0;
-        int h=nums.length-1;
-        
-        while(m<=h)
-        {
-            int x=nums[m];
-            if(x==0)
-            {
-                swap(nums,l,m);
-                l++;
-                m++;
-            }
-            else if(x==1)
-                m++;
-            
-            else{
-                swap(nums,m,h);
-                h--;
+        int i=0,k=0,j=nums.length-1;
+
+        while(k<=j) {
+            if(nums[k]==0) {
+                swap(nums,i,k);
+                i++;
+                k++;
+            } else if(nums[k]==1) {
+                k++;
+            } else {
+                swap(nums,k,j);
+                j--;
             }
         }
     }
-public void swap(int[] nums, int i, int j) {
-	int temp = nums[i];
-	nums[i] = nums[j];
-	nums[j] = temp;
-}
+
+    void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
 }
