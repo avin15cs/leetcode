9f6d62e43493d650823e7f2a4a28e31f81f40611
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-04 05:00:21 +0000

    Sync LeetCode submission - Valid Anagram (java)

--- leetcode-solutions/problems/valid_anagram/solution.java
+++ leetcode-solutions/problems/valid_anagram/solution.java
@@ -15,42 +15,42 @@ class Solution {
     //     return sCount.equals(tCount);        
     // }
 
-    // public boolean isAnagram(String s, String t) {
-    //     if (s.length() != t.length()) {
-    //         return false;
-    //     }
-
-    //     int[] ch=new int[26];
-    //     for(int i=0;i<s.length();i++) {
-    //         ch[s.charAt(i)-'a']++;
-    //         ch[t.charAt(i)-'a']--;
-    //     }
-
-    //     for(int i=0;i<26;i++) {
-    //         if(ch[i]!=0)
-    //             return false;
-    //     }
-
-    //     return true;
-
-    // }
-
-
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
             return false;
         }
 
-        char sc[]=s.toCharArray();
-        char tc[]=t.toCharArray();
-        Arrays.sort(sc);
-        Arrays.sort(tc);
+        int[] ch=new int[26];
+        for(int i=0;i<s.length();i++) {
+            ch[s.charAt(i)-'a']++;
+            ch[t.charAt(i)-'a']--;
+        }
 
-        int i=0;
-        int n=sc.length;
-        while(i<n && sc[i]==tc[i])
-            i++;
+        for(int i=0;i<26;i++) {
+            if(ch[i]!=0)
+                return false;
+        }
 
-        return i==n;
+        return true;
+
     }
+
+
+    // public boolean isAnagram(String s, String t) {
+    //     if (s.length() != t.length()) {
+    //         return false;
+    //     }
+
+    //     char sc[]=s.toCharArray();
+    //     char tc[]=t.toCharArray();
+    //     Arrays.sort(sc);
+    //     Arrays.sort(tc);
+
+    //     int i=0;
+    //     int n=sc.length;
+    //     while(i<n && sc[i]==tc[i])
+    //         i++;
+
+    //     return i==n;
+    // }
 }
