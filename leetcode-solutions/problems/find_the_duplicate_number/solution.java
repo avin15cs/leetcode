64c15d400db7800bac6cef6ca49d887085a8de86
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-04 07:56:40 +0000

    Sync LeetCode submission - Find the Duplicate Number (java)

--- leetcode-solutions/problems/find_the_duplicate_number/solution.java
+++ leetcode-solutions/problems/find_the_duplicate_number/solution.java
@@ -1,25 +1,19 @@
 class Solution {
     public int findDuplicate(int[] nums) {
         int i=0;
-
         while(i<nums.length) {
-            int correct = nums[i] - 1;
-            if(nums[i]!=nums[correct]) {
+            int pos=nums[i]-1;
+            if(nums[i]!=nums[pos]) {
                 int temp=nums[i];
-                nums[i]=nums[correct];
-                nums[correct]=temp;
+                nums[i]=nums[pos];
+                nums[pos]=temp;
             } else {
-                if(i!=correct)
+                if(i!=pos)
                     return nums[i];
                 i++;
             }
-                
         }
 
-        // for(i=0;i<nums.length;i++){
-        //     if(nums[i]!=i+1)    
-        //         return nums[i];
-        // }
         return -1;
     }
 }
