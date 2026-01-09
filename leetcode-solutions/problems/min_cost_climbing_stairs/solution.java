ffa3d2ef9e7b470b3660040ec4f9ff06e34e9cc9
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-09 15:13:07 +0000

    Sync LeetCode submission - Min Cost Climbing Stairs (java)

--- leetcode-solutions/problems/min_cost_climbing_stairs/solution.java
+++ leetcode-solutions/problems/min_cost_climbing_stairs/solution.java
@@ -1,34 +1,59 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int n=cost.length;
-        int prev1=cost[0];
-        int prev2=cost[1];
+    // public int minCostClimbingStairs(int[] cost) {
+    //     int n=cost.length;
+    //     int prev1=cost[0];
+    //     int prev2=cost[1];
         
-        if(n<2)
-            return Math.min(prev1,prev2);
+    //     if(n<2)
+    //         return Math.min(prev1,prev2);
 
-        int cur=0;
-        for(int i=2;i<n;i++) {
-            cur=cost[i]+Math.min(prev1,prev2);
-            prev1=prev2;
-            prev2=cur;
-        }
-        return Math.min(prev2,prev1);
-    }
+    //     int cur=0;
+    //     for(int i=2;i<n;i++) {
+    //         cur=cost[i]+Math.min(prev1,prev2);
+    //         prev1=prev2;
+    //         prev2=cur;
+    //     }
+    //     return Math.min(prev2,prev1);
+    // }
 
     // public int minCostClimbingStairs(int[] cost) {
-    //     int n = cost.length;
-    //     Integer[] dp = new Integer[n];
-    //     return Math.min(solve(cost, n - 1, dp),
-    //                     solve(cost, n - 2, dp));
+    //     int n=cost.length;
+    //     if(n==2)
+    //         return Math.min(cost[0],cost[1]);
+
+    //     Integer[] dp = new Integer[n+1];
+
+    //     return Math.min(minCost(cost, n-1, dp), minCost(cost, n-2, dp));
     // }
 
-    // private int solve(int[] cost, int i, Integer[] dp) {
-    //     if (i < 0) return 0;
-    //     if (dp[i] != null) return dp[i];
-    //     if (i == 0 || i == 1) return dp[i] = cost[i];
+    // private int minCost(int[] cost, int n, Integer[] dp) {
+    //     if(n<0)
+    //         return 0;
 
-    //     return dp[i] = cost[i] + Math.min(solve(cost, i - 1, dp),
-    //                                     solve(cost, i - 2, dp));
+    //     if(dp[n]!=null)
+    //         return dp[n];
+        
+    //     return cost[n]+ Math.min(minCost(cost,n-2,dp),minCost(cost,n-1,dp));
     // }
+
+    public int minCostClimbingStairs(int[] cost) {
+        if(cost == null || cost.length ==0) {
+            return 0;
+        }
+        
+        if(cost.length == 1) {
+            return cost[0];
+        }
+        
+        int prev1=cost[1];
+        int prev2=cost[0];
+
+        for(int i=2;i<cost.length;i++) {
+            int cur = Math.min(prev1,prev2)+cost[i];
+            prev2 = prev1;
+            prev1 = cur;
+        }
+
+        return Math.min(prev1, prev2);
+    }
 }
