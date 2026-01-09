054977ee3f3fa4f9c836ab34802c4c37974c75dc
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-09 16:14:27 +0000

    Sync LeetCode submission - House Robber (java)

--- leetcode-solutions/problems/house_robber/solution.java
+++ leetcode-solutions/problems/house_robber/solution.java
@@ -15,20 +15,35 @@ class Solution {
     // }
 
 
+    // public int rob(int[] nums) {
+    //     int n=nums.length;
+
+    //     if (n == 1) return nums[0];
+        
+    //     int prev1=0;
+    //     int prev2=0;
+
+    //     for(int num: nums) {
+    //         int rob=num + prev2;
+    //         int skip=prev1;
+
+    //         int cur=Math.max(rob, skip);
+
+    //         prev2=prev1;
+    //         prev1=cur;
+    //     }
+
+    //     return prev1;
+    // }
+
     public int rob(int[] nums) {
         int n=nums.length;
-
-        if (n == 1) return nums[0];
-        
-        int prev1=0;
-        int prev2=0;
-
-        for(int num: nums) {
-            int rob=num + prev2;
-            int skip=prev1;
-
-            int cur=Math.max(rob, skip);
-
+        if(n==1)
+            return nums[0];             
+        int prev2 = nums[0];
+        int prev1 = Math.max(nums[0], nums[1]);
+        for(int i=2;i<n;i++) {
+            int cur = Math.max(nums[i]+prev2, prev1);
             prev2=prev1;
             prev1=cur;
         }
