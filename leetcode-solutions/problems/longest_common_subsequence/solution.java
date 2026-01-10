45a9f02ad853738b6febe2aa28c13308be32b8c8
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-10 06:47:26 +0000

    Sync LeetCode submission - Longest Common Subsequence (java)

--- leetcode-solutions/problems/longest_common_subsequence/solution.java
+++ leetcode-solutions/problems/longest_common_subsequence/solution.java
@@ -1,26 +1,42 @@
 class Solution {
+    // public int longestCommonSubsequence(String text1, String text2) {
+    //     int n=text1.length();
+    //     int m=text2.length();
+
+    //     int[][] dp = new int[n+1][m+1];
+    //     for(int i=1;i<=n;i++) {
+    //         for(int j=1;j<=m;j++) {
+    //             if(text1.charAt(i-1)==text2.charAt(j-1))
+    //                 dp[i][j]=1+dp[i-1][j-1];
+    //             else
+    //                 dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
+    //         }
+    //     }
+    //     return dp[n][m];
+    // }
+
     public int longestCommonSubsequence(String text1, String text2) {
-        int n=text1.length();
-        int m=text2.length();
-        Integer[][] dp=new Integer[n+1][m+1];
+        int n = text1.length();
+        int m = text2.length();
 
-        return lcs(text1,text2,n,m,dp);
-    }
+        int[] dp = new int[m + 1];
 
-    int lcs(String s, String t, int n, int m, Integer[][] dp) {
-        if(n==0||m==0)
-            return 0;
-        
-        if(dp[n][m]!=null)
-            return dp[n][m];
-        
-        if(s.charAt(n-1)==t.charAt(m-1))
-            dp[n][m]=1+lcs(s,t,n-1,m-1,dp);
-        
-        else
-            dp[n][m]=Math.max(lcs(s,t,n-1,m,dp),lcs(s,t,n,m-1,dp));
-        
-        return dp[n][m];
+        for (int i = 1; i <= n; i++) {
+            int prev = 0;  // dp[i-1][j-1]
 
+            for (int j = 1; j <= m; j++) {
+                int temp = dp[j]; // dp[i-1][j]
+
+                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
+                    dp[j] = 1 + prev;
+                } else {
+                    dp[j] = Math.max(dp[j], dp[j - 1]);
+                }
+
+                prev = temp;
+            }
+        }
+
+        return dp[m];
     }
 }
