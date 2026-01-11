eac3262fc18fcbc0605f3fe8a9a1dcca388b619a
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-11 08:56:22 +0000

    Sync LeetCode submission - Merge Two Sorted Lists (java)

--- leetcode-solutions/problems/merge_two_sorted_lists/solution.java
+++ leetcode-solutions/problems/merge_two_sorted_lists/solution.java
@@ -9,33 +9,50 @@
  * }
  */
 class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    // public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
-        if(l1==null)
+    //     if(l1==null)
+    //         return l2;
+    //     if(l2==null)
+    //         return l1;
+
+    //     ListNode node=new ListNode(0);
+    //     ListNode dummy=node;
+
+    //     while(l1!=null && l2!=null) {
+    //         if(l1.val<l2.val) {
+    //             node.next=l1;
+    //             l1=l1.next;
+    //         } else {
+    //             node.next=l2;
+    //             l2=l2.next;
+    //         }
+    //         node=node.next;
+    //     }
+
+    //     if(l1!=null)
+    //         node.next=l1;
+        
+    //     if(l2!=null)
+    //         node.next=l2;
+
+    //     return dummy.next;
+    // }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if(l1 == null)
             return l2;
-        if(l2==null)
+        
+        if(l2 == null)
             return l1;
 
-        ListNode node=new ListNode(0);
-        ListNode dummy=node;
-
-        while(l1!=null && l2!=null) {
-            if(l1.val<l2.val) {
-                node.next=l1;
-                l1=l1.next;
-            } else {
-                node.next=l2;
-                l2=l2.next;
-            }
-            node=node.next;
+        if(l1.val<l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-
-        if(l1!=null)
-            node.next=l1;
-        
-        if(l2!=null)
-            node.next=l2;
-
-        return dummy.next;
     }
+
 }
