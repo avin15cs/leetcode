4676a9137d49dfc838be52d20bfde1b5324e6e1c
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-13 02:30:48 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- leetcode-solutions/problems/valid_sudoku/solution.java
+++ leetcode-solutions/problems/valid_sudoku/solution.java
@@ -1,40 +1,64 @@
 class Solution {
+    // public boolean isValidSudoku(char[][] board) {
+    //     int rows = board.length;
+    //     int cols = board[0].length;
+
+    //     for (int r = 0; r < rows; r++) {
+    //         for (int c = 0; c < cols; c++) {
+    //             if (board[r][c] != '.' && !isValid(board, r, c)) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // private boolean isValid(char[][] board, int r, int c) {
+    //     char val = board[r][c];
+
+    //     // Check row
+    //     for (int col = 0; col < 9; col++) {
+    //         if (col != c && board[r][col] == val) return false;
+    //     }
+
+    //     // Check column
+    //     for (int row = 0; row < 9; row++) {
+    //         if (row != r && board[row][c] == val) return false;
+    //     }
+
+    //     // Check 3x3 sub-grid
+    //     int boxRow = (r / 3) * 3;
+    //     int boxCol = (c / 3) * 3;
+
+    //     for (int i = boxRow; i < boxRow + 3; i++) {
+    //         for (int j = boxCol; j < boxCol + 3; j++) {
+    //             if (!(i == r && j == c) && board[i][j] == val) return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
     public boolean isValidSudoku(char[][] board) {
-        int r=board[0].length;
-        int c=board.length;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(board[i][j] != '.' && !isValid(board,i,j))
-                    return false;
-            }
-        }
+        int row[] = new int[9];
+            int col[] = new int[9];
+            int box[] = new int[9];
 
-        return true;
-    }
-
-    private boolean isValid(char[][] board, int r, int c){
-        for(int i=0;i<board.length;i++){
-            if(i!=c && board[r][i]==board[r][c])
-                return false;
-        }
-
-        for(int j=0;j<board[0].length;j++){
-            if(j!=r && board[j][c]==board[r][c])
-                return false;
-        }
-
-            // Check 3x3 sub-box
-        int boxRow = (r / 3) * 3;
-        int boxCol = (c / 3) * 3;
-    
-        for (int i = boxRow; i < boxRow + 3; i++) {
-            for (int j = boxCol; j < boxCol + 3; j++) {
-                if (i != r && j != c && board[i][j] == board[r][c]) {
-                    return false;
+            for(int i=0;i<9;i++){
+                for(int j=0;j<9;j++){
+                    char c = board[i][j];
+                    if(c != '.'){
+                        int bit = 1 << (c - '0');
+                        int boxN = (i / 3) * 3 + (j / 3);
+                        if((row[i] & bit) != 0 || (col[j] & bit) != 0 || (box[boxN] & bit) !=0)return false;
+                        row[i] |= bit;
+                        col[j] |=bit;
+                        box[boxN] |= bit;
+                    }
+                    
                 }
             }
-        }
-
         return true;
     }
+
 }
