c6d3350493e0b5b82d308aaf53a22088f40c3c76
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-14 03:21:00 +0000

    Sync LeetCode submission - Max Area of Island (java)

--- leetcode-solutions/problems/max_area_of_island/solution.java
+++ leetcode-solutions/problems/max_area_of_island/solution.java
@@ -3,7 +3,7 @@ class Solution {
         int ans = 0;
         for (int i = 0; i < grid.length; i++) 
             for (int j = 0; j < grid[0].length; j++)
-                if (grid[i][j] != 0) 
+                if (grid[i][j] == 1) 
                     ans = Math.max(ans, trav(i, j, grid));
         return ans;
     }
