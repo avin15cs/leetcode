20c8a9d9a3f2aa68a77de0b56a758ac74b41be7d
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-19 02:09:33 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- leetcode-solutions/problems/valid_sudoku/solution.java
+++ leetcode-solutions/problems/valid_sudoku/solution.java
@@ -39,26 +39,68 @@ class Solution {
     //     return true;
     // }
 
-    public boolean isValidSudoku(char[][] board) {
-        int row[] = new int[9];
-            int col[] = new int[9];
-            int box[] = new int[9];
+    // public boolean isValidSudoku(char[][] board) {
+    //     int row[] = new int[9];
+    //     int col[] = new int[9];
+    //     int box[] = new int[9];
 
-            for(int i=0;i<9;i++){
-                for(int j=0;j<9;j++){
-                    char c = board[i][j];
-                    if(c != '.'){
-                        int bit = 1 << (c - '0');
-                        int boxN = (i / 3) * 3 + (j / 3);
-                        if((row[i] & bit) != 0 || (col[j] & bit) != 0 || (box[boxN] & bit) !=0)return false;
-                        row[i] |= bit;
-                        col[j] |=bit;
-                        box[boxN] |= bit;
-                    }
-                    
+    //     for(int i=0;i<9;i++){
+    //         for(int j=0;j<9;j++){
+    //             char c = board[i][j];
+    //             if(c != '.'){
+    //                 int bit = 1 << (c - '0');
+    //                 int boxN = (i / 3) * 3 + (j / 3);
+    //                 if((row[i] & bit) != 0 || (col[j] & bit) != 0 || (box[boxN] & bit) !=0)
+    //                     return false;
+    //                 row[i] |= bit;
+    //                 col[j] |=bit;
+    //                 box[boxN] |= bit;
+    //             }            
+    //         }
+    //     }
+    //     return true;
+    // }
+
+    // public boolean isValidSudoku(char[][] board) {
+
+    //     Set<String> visited = new HashSet<>();
+
+    //     for(int i=0;i<board.length;i++) {
+    //         for(int j=0;j<board[0].length;j++) {
+
+    //             if (board[i][j] == '.') continue; 
+    //             if(!(visited.add(i+"row"+board[i][j]) && visited.add(j+"col"+board[i][j])
+    //                 && visited.add(((i/3)*3+j/3)+"box"+board[i][j])))
+    //                 return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
+
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                char ch = board[r][c];
+                if (ch == '.') continue;
+
+                int num = ch - '1';
+                int boxIndex = (r / 3) * 3 + (c / 3);
+
+                if (rows[r][num] || cols[c][num] || boxes[boxIndex][num]) {
+                    return false;
                 }
+
+                rows[r][num] = true;
+                cols[c][num] = true;
+                boxes[boxIndex][num] = true;
             }
+        }
         return true;
     }
-
 }
