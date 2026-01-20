1ac1578044881b1dba90bf31cf2ff6f7a07596e2
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-20 03:48:03 +0000

    Sync LeetCode submission - LRU Cache (java)

--- leetcode-solutions/problems/lru_cache/solution.java
+++ leetcode-solutions/problems/lru_cache/solution.java
@@ -1,106 +1,69 @@
-// class LRUCache {
-//     private class Node {
-//         int key, value;
-//         Node prev, next;
-//         Node(int k, int v) {
-//             key = k;
-//             value = v;
-//         }
-//     }
-
-//     private HashMap<Integer,Node> cache;
-//     private Node head,tail;
-//     private int capacity;
-//     public LRUCache(int capacity) {
-//         this.capacity=capacity;
-//         cache=new HashMap<>();
-//         head=new Node(0,0);
-//         tail=new Node(0,0);
-//         head.next=tail;
-//         tail.prev=head;
-//     }
-    
-//     public int get(int key) {
-//         if(!cache.containsKey(key))
-//             return -1;
-
-//         Node node=cache.get(key);
-
-//         remove(node);
-//         insertToHead(node);
-
-//         return node.value;
-
-//     }
-    
-//     public void put(int key, int value) {
-        
-//         if(cache.containsKey(key)) {
-//             Node node=cache.get(key);
-//             node.value=value;
-
-//             remove(node);
-//             insertToHead(node);
-//         } else {
-//             Node node = new Node(key, value);
-//             cache.put(key,node);
-//             insertToHead(node);
-
-//             if(cache.size()>capacity) {
-//                 Node tnode = tail.prev;
-//                 remove(tnode);
-
-//                 cache.remove(tnode.key);
-//             }
-//         }
-//     }
-
-//     private void remove(Node node) {
-//         node.prev.next = node.next;
-//         node.next.prev = node.prev;
-//     }
-
-//     private void insertToHead(Node node) {
-//         node.next = head.next;
-//         node.prev = head;
-//         head.next.prev = node;
-//         head.next = node;
-//     }
-// }
-
 class LRUCache {
-
-    int capacity;
-    Queue<Integer> q = new ArrayDeque<>();
-    Map<Integer, Integer> map = new HashMap<>();
-
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
+    private class Node {
+        int key, value;
+        Node prev, next;
+        Node(int k, int v) {
+            key = k;
+            value = v;
+        }
     }
 
+    private HashMap<Integer,Node> cache;
+    private Node head,tail;
+    private int capacity;
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new HashMap<>();
+        head = new Node(0,0);
+        tail = new Node(0,0);
+        head.next = tail;
+        tail.prev = head;
+    }
+    
     public int get(int key) {
-        if (!map.containsKey(key)) {
+        if(!cache.containsKey(key)) {
             return -1;
         }
 
-        q.remove(key);     // O(n)
-        q.offer(key);      // MRU
-        return map.get(key);
+        Node node = cache.get(key);
+        remove(node);
+        addToHead(node);
+
+        return node.value;
     }
-
+    
     public void put(int key, int value) {
+        if(cache.containsKey(key)) {
+            Node node = cache.get(key);
+            node.value = value;
 
-        if (map.containsKey(key)) {
-            q.remove(key);
-            q.offer(key);
-        } else {
-            if (map.size() == capacity) {
-                int lruKey = q.poll();
-                map.remove(lruKey);
-            }
-            q.offer(key);
+            remove(node);
+            addToHead(node);
+
+            return;
         }
 
-        map.put(key, value);
+        if(cache.size()==capacity) {
+            Node node = tail.prev;
+            remove(node);
+            cache.remove(node.key);
+        }
+
+        Node node = new Node(key, value);
+        cache.put(key,node);
+        addToHead(node);
+    }
+
+    private void remove(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void addToHead(Node node) {
+        node.next = head.next;
+        node.prev = head;
+
+        head.next.prev = node;
+        head.next = node;
     }
 }
