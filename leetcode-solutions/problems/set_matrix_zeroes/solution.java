25098ab20b9150be5be801e50fc6359f0e1bf4a9
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-21 03:26:48 +0000

    Sync LeetCode submission - Set Matrix Zeroes (java)

--- leetcode-solutions/problems/set_matrix_zeroes/solution.java
+++ leetcode-solutions/problems/set_matrix_zeroes/solution.java
@@ -1,51 +1,23 @@
 class Solution {
-//     public void setZeroes(int[][] matrix) {
-
-//         boolean[] rows=new boolean[matrix.length];
-//         boolean[] cols=new boolean[matrix[0].length];
-
-//         for(int i=0;i<matrix.length;i++) {
-//             for(int j=0;j<matrix[0].length;j++) {
-//                 if(matrix[i][j]==0) {
-//                     rows[i]=true;
-//                     cols[j]=true;
-//                 }                   
-//             }
-//         }
-
-//         for(int i=0;i<matrix.length;i++) {
-//             for(int j=0;j<matrix[0].length;j++) {
-//                 if(rows[i]||cols[j])
-//                     matrix[i][j]=0;
-//             }
-//         }
-//     }
-
     public void setZeroes(int[][] matrix) {
+
+        boolean[] rows=new boolean[matrix.length];
+        boolean[] cols=new boolean[matrix[0].length];
+
         for(int i=0;i<matrix.length;i++) {
             for(int j=0;j<matrix[0].length;j++) {
-                if(matrix[i][j]==0) 
-                    setMinusOne(matrix,i,j);
+                if(matrix[i][j]==0) {
+                    rows[i]=true;
+                    cols[j]=true;
+                }                   
             }
         }
 
         for(int i=0;i<matrix.length;i++) {
             for(int j=0;j<matrix[0].length;j++) {
-                if(matrix[i][j]==99999) 
+                if(rows[i]||cols[j])
                     matrix[i][j]=0;
             }
-        }
-    }
-
-    void setMinusOne(int[][] matrix,int n, int m) {
-        for(int i=0;i<matrix[0].length;i++) {
-            if(matrix[n][i]!=0)
-                matrix[n][i]=99999;
-        }
-
-        for(int j=0;j<matrix.length;j++) {
-            if(matrix[j][m]!=0)
-                matrix[j][m]=99999;
         }
     }
 }
