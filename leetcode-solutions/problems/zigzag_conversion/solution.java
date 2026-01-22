9773c9d8116973b16642c7ef6f28560588e543f9
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-22 03:23:31 +0000

    Sync LeetCode submission - Zigzag Conversion (java)

--- leetcode-solutions/problems/zigzag_conversion/solution.java
+++ leetcode-solutions/problems/zigzag_conversion/solution.java
@@ -26,38 +26,64 @@ class Solution {
     //     return res.toString();
     // }
 
+    // public String convert(String s, int numRows) {
+
+    //     if (numRows == 1 || s.length() <= numRows) {
+    //         return s;
+    //     }
+
+    //     StringBuilder[] rows = new StringBuilder[numRows];
+    //     for (int i = 0; i < numRows; i++) {
+    //         rows[i] = new StringBuilder();
+    //     }
+
+    //     int currentRow = 0;
+    //     boolean goingDown = true;
+
+    //     for (char ch : s.toCharArray()) {
+    //         rows[currentRow].append(ch);
+
+    //         if (currentRow == 0) {
+    //             goingDown = true;
+    //         } else if (currentRow == numRows - 1) {
+    //             goingDown = false;
+    //         }
+
+    //         currentRow += goingDown ? 1 : -1;
+    //     }
+
+    //     StringBuilder result = new StringBuilder();
+    //     for (StringBuilder row : rows) {
+    //         result.append(row);
+    //     }
+
+    //     return result.toString();
+    // }
+
+
     public String convert(String s, int numRows) {
-
-        if (numRows == 1 || s.length() <= numRows) {
+        if(numRows == 1)
             return s;
+
+        List<StringBuilder> rows = new ArrayList<>();
+
+        for(int i=0;i<numRows;i++)
+            rows.add(new StringBuilder());
+
+        int i=0;
+        while(i<s.length()) {
+
+            for(int down = 0;down < numRows && i<s.length();down++)
+                rows.get(down).append(s.charAt(i++));
+            
+            for(int up = numRows-2;up>0 && i<s.length();up--)
+                rows.get(up).append(s.charAt(i++));
         }
 
-        StringBuilder[] rows = new StringBuilder[numRows];
-        for (int i = 0; i < numRows; i++) {
-            rows[i] = new StringBuilder();
-        }
-
-        int currentRow = 0;
-        boolean goingDown = true;
-
-        for (char ch : s.toCharArray()) {
-            rows[currentRow].append(ch);
-
-            if (currentRow == 0) {
-                goingDown = true;
-            } else if (currentRow == numRows - 1) {
-                goingDown = false;
-            }
-
-            currentRow += goingDown ? 1 : -1;
-        }
-
-        StringBuilder result = new StringBuilder();
-        for (StringBuilder row : rows) {
-            result.append(row);
-        }
-
-        return result.toString();
+        StringBuilder ans = new StringBuilder();
+        for(StringBuilder sb: rows)
+            ans.append(sb);
+        
+        return ans.toString();
     }
-
 }
