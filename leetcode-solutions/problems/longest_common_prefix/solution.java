e68ef703258fcd2dabe26d43a803f3731b4e6ad7
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-22 03:28:17 +0000

    Sync LeetCode submission - Longest Common Prefix (java)

--- leetcode-solutions/problems/longest_common_prefix/solution.java
+++ leetcode-solutions/problems/longest_common_prefix/solution.java
@@ -1,21 +1,41 @@
 class Solution {
+    // public String longestCommonPrefix(String[] strs) {
+    //     Arrays.sort(strs);
+
+    //     StringBuilder cp=new StringBuilder();
+    //     int n=strs.length;
+    //     int l0=strs[0].length();
+    //     int ln=strs[n-1].length();
+    //     int i=0,j=0;
+    //     while(i<l0 && j<ln){
+    //         if(strs[0].charAt(i)!=strs[n-1].charAt(j))
+    //             break;
+    //         cp.append(strs[0].charAt(i));
+
+    //         i++;
+    //         j++;
+    //     }
+
+    //     return cp.toString();
+    // }
+
     public String longestCommonPrefix(String[] strs) {
-        Arrays.sort(strs);
 
-        StringBuilder cp=new StringBuilder();
-        int n=strs.length;
-        int l0=strs[0].length();
-        int ln=strs[n-1].length();
-        int i=0,j=0;
-        while(i<l0 && j<ln){
-            if(strs[0].charAt(i)!=strs[n-1].charAt(j))
-                break;
-            cp.append(strs[0].charAt(i));
-
-            i++;
-            j++;
+        if (strs == null || strs.length == 0) {
+            return "";
         }
 
-        return cp.toString();
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                if (i >= strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+
+        return strs[0];
     }
+
 }
