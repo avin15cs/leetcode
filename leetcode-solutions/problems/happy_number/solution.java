61a3b2b1c448c4f2cf0182cbd9800dd5443f01d7
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-24 12:39:43 +0000

    Sync LeetCode submission - Happy Number (java)

--- leetcode-solutions/problems/happy_number/solution.java
+++ leetcode-solutions/problems/happy_number/solution.java
@@ -1,16 +1,15 @@
 class Solution {
     public boolean isHappy(int n) {
         Set<Integer> seen=new HashSet<>();
-
-        while(n!=1 && !seen.contains(n)){
+        while(n!=1 && !seen.contains(n)) {
             seen.add(n);
-            int sum=0,rem=0;
-            while(n!=0){
-                rem=n%10;
-                sum=sum+rem*rem;
-                n=n/10;
+            int rem = 0, sum = 0;
+            while(n!=0) {
+                rem = n%10;
+                sum = sum+rem*rem;
+                n = n/10;
             }
-            n=sum;
+            n = sum;
         }
 
         return n==1;
