6995a3c7a35cb4659fe0fb2b860ecc82ae307e25
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-26 06:42:24 +0000

    Sync LeetCode submission - Find the Duplicate Number (java)

--- leetcode-solutions/problems/find_the_duplicate_number/solution.java
+++ leetcode-solutions/problems/find_the_duplicate_number/solution.java
@@ -1,19 +1,64 @@
 class Solution {
+    // public int findDuplicate(int[] nums) {
+    //     int i=0;
+    //     while(i<nums.length) {
+    //         int pos=nums[i]-1;
+        
+    //         if(nums[i]!=nums[pos]) {
+    //             int temp = nums[i];
+    //             nums[i]=nums[pos];
+    //             nums[pos]=temp;
+    //         } else {
+    //             if(i!=pos)
+    //                 return nums[i];
+    //             i++;
+    //         }
+
+    //     }
+
+    //     return -1;
+    // }
+
+    // public int findDuplicate(int[] nums) {
+    //     int slow = nums[0];
+    //     int fast = nums[0];
+
+    //     // Phase 1: Detect cycle
+    //     do {
+    //         slow = nums[slow];
+    //         fast = nums[nums[fast]];
+    //     } while (slow != fast);
+
+    //     // Phase 2: Find entry of cycle
+    //     slow = nums[0];
+    //     while (slow != fast) {
+    //         slow = nums[slow];
+    //         fast = nums[fast];
+    //     }
+
+    //     return fast;
+    // }
+
+
     public int findDuplicate(int[] nums) {
-        int i=0;
-        while(i<nums.length) {
-            int pos=nums[i]-1;
-            if(nums[i]!=nums[pos]) {
-                int temp=nums[i];
-                nums[i]=nums[pos];
-                nums[pos]=temp;
+        int low = 1, high = nums.length - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+
+            for (int num : nums) {
+                if (num <= mid) count++;
+            }
+
+            if (count > mid) {
+                high = mid;
             } else {
-                if(i!=pos)
-                    return nums[i];
-                i++;
+                low = mid + 1;
             }
         }
 
-        return -1;
+        return low;
     }
+
 }
