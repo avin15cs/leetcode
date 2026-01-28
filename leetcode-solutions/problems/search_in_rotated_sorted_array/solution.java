4fb6f19d9b919f79772957a94f6205f0e8423341
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-28 23:27:41 +0000

    Sync LeetCode submission - Search in Rotated Sorted Array (java)

--- leetcode-solutions/problems/search_in_rotated_sorted_array/solution.java
+++ leetcode-solutions/problems/search_in_rotated_sorted_array/solution.java
@@ -18,13 +18,14 @@ class Solution {
         while(l<=h) {
             int mid=l+(h-l)/2;
             if(nums[0]>nums[mid]){
-                ans=mid;
+                // ans=mid;
                 h=mid-1;
             }
             else
                 l=mid+1;
         }
-        return ans;
+        // return ans;
+        return l;
     }
 
 
