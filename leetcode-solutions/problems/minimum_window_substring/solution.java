13f58737b04cde38e49531a30a2f95b648646958
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-01-30 01:47:29 +0000

    Sync LeetCode submission - Minimum Window Substring (java)

--- leetcode-solutions/problems/minimum_window_substring/solution.java
+++ leetcode-solutions/problems/minimum_window_substring/solution.java
@@ -1,45 +1,103 @@
 class Solution {
+    // public String minWindow(String s, String t) {
+    // int sl = s.length();
+    // int tl = t.length();
+    // if (sl < tl) return "";
+
+    // int i = 0, j = 0;
+
+    // Map<Character, Integer> tmap = new HashMap<>();
+    // for (int k = 0; k < tl; k++) {
+    //     char c = t.charAt(k);
+    //     tmap.put(c, tmap.getOrDefault(c, 0) + 1);
+    // }
+
+    // int size = tmap.size();  // number of unique chars needed
+    // Map<Character, Integer> smap = new HashMap<>();
+    // int reqSize = 0;
+
+    // int minSize = Integer.MAX_VALUE;
+    // int start = 0;
+
+    // while (j < sl) {
+
+    //     char c = s.charAt(j);
+
+    //     // INSERT ALWAYS
+    //     smap.put(c, smap.getOrDefault(c, 0) + 1);
+
+    //     // MATCH ONLY IF RELEVANT
+    //     if (tmap.containsKey(c) &&
+    //         smap.get(c).intValue() == tmap.get(c).intValue()) {
+    //         reqSize++;
+    //     }
+
+    //     // SHRINK
+    //     while (reqSize == size) {
+
+    //         if (j - i + 1 < minSize) {
+    //             minSize = j - i + 1;
+    //             start = i;
+    //         }
+
+    //         char ci = s.charAt(i);
+
+    //         // REMOVE ALWAYS
+    //         smap.put(ci, smap.get(ci) - 1);
+
+    //         // BREAK MATCH ONLY IF RELEVANT
+    //         if (tmap.containsKey(ci) &&
+    //             smap.get(ci) < tmap.get(ci)) {
+    //             reqSize--;
+    //         }
+
+    //         i++;
+    //     }
+
+    //     j++;
+    // }
+
+    // return minSize == Integer.MAX_VALUE
+    //         ? ""
+    //         : s.substring(start, start + minSize);
+    // }
+
     public String minWindow(String s, String t) {
-        int n=s.length();
-        int m=t.length();
+        int ls = s.length();
+        int lt = t.length();
 
-        if(m>n)
-            return "";
+        Map<Character, Integer> tmap = new HashMap<>();
 
-        HashMap<Character,Integer> smap=new HashMap<>();
-        HashMap<Character,Integer> tmap=new HashMap<>();
-
-        for(char c:t.toCharArray()) {
-            tmap.put(c,tmap.getOrDefault(c,0)+1);
+        for(int i=0;i<lt;i++) {
+            char c = t.charAt(i);
+            tmap.put(c,tmap.getOrDefault(c, 0)+1);
         }
 
-        int size=tmap.size();
-        int reqSize=0;
-        int i=0,j=0;
-        int min=Integer.MAX_VALUE;
-        int start=i;
-        while(j<n) {
-            char c=s.charAt(j);
-            smap.put(c,smap.getOrDefault(c,0)+1);
+        Map<Character, Integer> smap = new HashMap<>();
 
-            if(tmap.containsKey(c) && tmap.get(c).intValue()==smap.get(c).intValue())
-                reqSize++;
+        int i=0, j=0, min=Integer.MAX_VALUE;
+        int tsize = tmap.size(), size = 0;
+        int start = 0;
+        while(j<ls) {
+            char c = s.charAt(j);
+            smap.put(c, smap.getOrDefault(c,0)+1);
+            if(tmap.containsKey(c) && smap.get(c).intValue()==tmap.get(c).intValue())
+                size++;
 
-            while(size==reqSize) {
-
+            while(size == tsize) {
                 if(min>j-i+1) {
-                    min=j-i+1;
-                    start=i;
+                    min = j-i+1;
+                    start = i;
                 }
-                char ch=s.charAt(i);
-                smap.put(ch,smap.get(ch)-1);
-                
-                if(tmap.containsKey(ch) && tmap.get(ch)>smap.get(ch))
-                    reqSize--;
+                char ch = s.charAt(i);
+                smap.put(ch, smap.get(ch)-1);
                 i++;
+                if(tmap.containsKey(ch) && tmap.get(ch)>smap.get(ch))
+                    size--;
             }
             j++;
         }
-        return min==Integer.MAX_VALUE?"":s.substring(start,start+min);
+
+        return (min==Integer.MAX_VALUE)?"":s.substring(start, start+min);
     }
 }
