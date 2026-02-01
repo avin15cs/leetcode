eccc4e78cb959f30a02c812ad93f3cfdb0eb1da7
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-01 15:31:16 +0000

    Sync LeetCode submission - Daily Temperatures (java)

---
+++ leetcode-solutions/problems/daily_temperatures/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for(int i=n-1;i>=0;i--) {
+            while(!stack.isEmpty() && temperatures[stack.peek()]<=temperatures[i])
+                stack.pop();
+            
+            ans[i] = stack.isEmpty()?0:stack.peek()-i;
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
