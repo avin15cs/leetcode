0e75ee67d60cb607f4aeea2ee050e3ee2bfd3355
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-03 00:40:44 +0000

    Sync LeetCode submission - Balanced Binary Tree (java)

--- leetcode-solutions/problems/balanced_binary_tree/solution.java
+++ leetcode-solutions/problems/balanced_binary_tree/solution.java
@@ -15,26 +15,37 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root==null) return true;
-
-        return checkBalance(root)!=-1;
+        return height(root)!=-1;
     }
 
-    private int checkBalance(TreeNode root) {
-        if(root==null)  return 0;
+    // private int height(TreeNode root) {
+    //     if(root==null)  return 0;
 
-        int left=checkBalance(root.left);
-        if(left == -1)
+    //     int left=height(root.left);
+    //     if(left == -1)
+    //         return -1;
+
+    //     int right=height(root.right);
+    //     if(right == -1)
+    //         return -1;
+
+    //     if(Math.abs(left-right)>1)
+    //         return -1;
+
+    //     return 1+Math.max(left, right);
+    // }
+
+    private int height(TreeNode root) {
+        if(root == null)
+            return 0;
+
+        int left = height(root.left);
+        int right = height(root.right);
+
+        if(left==-1||right==-1||Math.abs(left-right)>1)
             return -1;
-
-        int right=checkBalance(root.right);
-        if(right == -1)
-            return -1;
-
-        if(Math.abs(left-right)>1)
-            return -1;
-
-        return 1+Math.max(left, right);
+        
+        return 1 + Math.max(left,right);
     }
 
 }
