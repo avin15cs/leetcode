c6f126a91b350bc6264830bab9670b1e53ab4404
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-03 03:46:08 +0000

    Sync LeetCode submission - Subsets II (java)

--- leetcode-solutions/problems/subsets_ii/solution.java
+++ leetcode-solutions/problems/subsets_ii/solution.java
@@ -1,29 +1,49 @@
 class Solution {
+    // public List<List<Integer>> subsetsWithDup(int[] nums) {
+    //     int n=nums.length;
+    //     Arrays.sort(nums);
+    //     return solve(nums, new ArrayList<>(), n-1);
+    // }
+
+    // List<List<Integer>> solve(int[] nums, List<Integer> cur, int n) {
+    //     if(n<0) {
+    //         List<List<Integer>> ans=new ArrayList<>();
+    //         ans.add(new ArrayList<>(cur));
+    //         return ans;
+    //     }
+
+    //     int next=n-1;
+    //     while(next>=0 && nums[n]==nums[next]){
+    //         next--;
+    //     }
+
+    //     List<List<Integer>> left=solve(nums,cur,next);
+
+    //     cur.add(nums[n]);
+    //     List<List<Integer>> right=solve(nums,cur,n-1);
+    //     cur.remove(cur.size()-1);
+    //     left.addAll(right);
+
+    //     return left;
+    // }
+
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        int n=nums.length;
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        return solve(nums, new ArrayList<>(), n-1);
+        backtrack(res, new ArrayList<>(), nums, 0);
+        return res;
     }
 
-    List<List<Integer>> solve(int[] nums, List<Integer> cur, int n) {
-        if(n<0) {
-            List<List<Integer>> ans=new ArrayList<>();
-            ans.add(new ArrayList<>(cur));
-            return ans;
+    void backtrack(List<List<Integer>> res, List<Integer> cur, int[] nums, int idx) {
+        res.add(new ArrayList<>(cur));
+
+        for(int i=idx;i<nums.length;i++) {
+            if(i>idx && nums[i]==nums[i-1])
+                continue;
+
+            cur.add(nums[i]);
+            backtrack(res, cur, nums, i+1);
+            cur.remove(cur.size()-1);
         }
-
-        int next=n-1;
-        while(next>=0 && nums[n]==nums[next]){
-            next--;
-        }
-
-        List<List<Integer>> left=solve(nums,cur,next);
-
-        cur.add(nums[n]);
-        List<List<Integer>> right=solve(nums,cur,n-1);
-        cur.remove(cur.size()-1);
-        left.addAll(right);
-
-        return left;
     }
 }
