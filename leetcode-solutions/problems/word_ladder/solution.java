4e597ee22444449fa14420ab02198f353a0a1918
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-04 14:54:51 +0000

    Sync LeetCode submission - Word Ladder (java)

--- leetcode-solutions/problems/word_ladder/solution.java
+++ leetcode-solutions/problems/word_ladder/solution.java
@@ -1,12 +1,51 @@
 class Solution {
+    // public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+    //     Set<String> dict = new HashSet<>(wordList); 
+    //     if(!dict.contains(endWord))
+    //         return 0;
+
+    //     Queue<String> q=new LinkedList<>();
+
+    //     q.add(beginWord);
+
+    //     int len=0;
+
+    //     while(!q.isEmpty()) {
+    //         int size = q.size();
+    //         len++;
+
+    //         for(int i=0;i<size;i++) {
+    //             String current = q.remove();
+
+    //             for(int j=0;j<current.length();j++) {
+    //                 char[] temp = current.toCharArray();
+    //                 for(char c='a';c<='z';c++) {
+    //                     temp[j]=c;
+    //                     String newWord = new String(temp);
+    //                     if(newWord.equals(endWord))
+    //                         return len+1;
+                        
+    //                     if(dict.contains(newWord)) {
+    //                         q.add(newWord);
+    //                         dict.remove(newWord);
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+
+    //     return 0;
+    // }
+
+
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> dict = new HashSet<>(wordList); 
-        if(!dict.contains(endWord))
+
+        Set<String> set = new HashSet<>(wordList);
+
+        if(!set.contains(endWord))
             return 0;
 
-        // Set<String> visited=new HashSet<>();
-        Queue<String> q=new LinkedList<>();
-
+        Queue<String> q = new LinkedList<>();
         q.add(beginWord);
 
         int len=0;
@@ -14,28 +53,27 @@ class Solution {
         while(!q.isEmpty()) {
             int size = q.size();
             len++;
-
             for(int i=0;i<size;i++) {
-                String current = q.remove();
+                String cur = q.remove();
 
-                for(int j=0;j<current.length();j++) {
-                    char[] temp = current.toCharArray();
+                for(int j=0;j<cur.length();j++) {
+                    char[] temp=cur.toCharArray();
+
                     for(char c='a';c<='z';c++) {
+                        if (c == cur.charAt(j)) continue;
                         temp[j]=c;
                         String newWord = new String(temp);
                         if(newWord.equals(endWord))
                             return len+1;
                         
-                        if(dict.contains(newWord)) {
+                        if(set.contains(newWord)) {
                             q.add(newWord);
-                            dict.remove(newWord);
-                            // visited.add(newWord);
+                            set.remove(newWord);
                         }
                     }
                 }
             }
         }
-
         return 0;
     }
 }
