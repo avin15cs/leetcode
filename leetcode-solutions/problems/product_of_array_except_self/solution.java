c8d0e36cb2f25fcbe2faab3ff59c37564018100d
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-06 02:10:13 +0000

    Sync LeetCode submission - Product of Array Except Self (java)

--- leetcode-solutions/problems/product_of_array_except_self/solution.java
+++ leetcode-solutions/problems/product_of_array_except_self/solution.java
@@ -1,17 +1,18 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n=nums.length;
-        int [] res=new int[n];
-        res[0]=1;
+        int[] ans = new int[n];
+        ans[0]=1;
         for(int i=1;i<n;i++) {
-            res[i]=nums[i-1]*res[i-1];
+            ans[i]=ans[i-1]*nums[i-1];
         }
 
-        int r=1;
+        int p=1;
         for(int i=n-2;i>=0;i--) {
-            r=r*nums[i+1];
-            res[i]=res[i]*r;
+            p=p*nums[i+1];
+            ans[i]=p*ans[i];
         }
-        return res;
+
+        return ans;
     }
 }
