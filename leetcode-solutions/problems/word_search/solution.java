0e4e3a5c7c2928c31fd69ab919e8ce2f6ca8da95
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-07 06:43:41 +0000

    Sync LeetCode submission - Word Search (java)

--- leetcode-solutions/problems/word_search/solution.java
+++ leetcode-solutions/problems/word_search/solution.java
@@ -1,29 +1,29 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        
         for(int i=0;i<board.length;i++) {
             for(int j=0;j<board[0].length;j++) {
-                if(exists(board,i,j,word,0))
+                if(exists(board, word, i, j, 0))
                     return true;
             }
         }
+
         return false;
     }
 
-    private boolean exists(char[][] board, int i, int j, String word, int idx) {
-        if(idx==word.length())
+    boolean exists(char[][] board, String word, int r, int c, int idx) {
+        if(idx == word.length())
             return true;
-        
-        if(i>=board.length||j>=board[0].length||i<0||j<0||board[i][j]!=word.charAt(idx)) 
-            return false;
-        
-        board[i][j]='*';
-        boolean result = exists(board,i+1,j,word,idx+1)||
-                        exists(board,i-1,j,word,idx+1)||
-                        exists(board,i,j+1,word,idx+1)||
-                        exists(board,i,j-1,word,idx+1);
-        board[i][j]=word.charAt(idx);
 
+        if(r<0||c<0||r==board.length||c==board[0].length||board[r][c]!=word.charAt(idx))
+            return false;
+
+        board[r][c]='*';
+        boolean result=exists(board,word,r+1,c,idx+1)||
+                        exists(board,word,r-1,c,idx+1)||
+                        exists(board,word,r,c+1,idx+1)||
+                        exists(board,word,r,c-1,idx+1);
+        board[r][c]=word.charAt(idx);
         return result;
     }
-
 }
