bb6abc9f7a8c54a0643579fd8885ece0722bd0f8
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-07 11:58:17 +0000

    Sync LeetCode submission - Word Break (java)

--- leetcode-solutions/problems/word_break/solution.java
+++ leetcode-solutions/problems/word_break/solution.java
@@ -6,21 +6,37 @@ class Solution {
         return solve(0,s,dict,dp);
     }
 
-    boolean solve(int idx, String s, Set<String> dict, Boolean[] dp) {
-        if(idx==s.length())
+    // boolean solve(int idx, String s, Set<String> dict, Boolean[] dp) {
+    //     if(idx==s.length())
+    //         return true;
+
+    //     if(dp[idx]!=null)
+    //         return dp[idx];
+
+    //     for(String word: dict) {
+    //         if(s.startsWith(word,idx)) {
+    //             if(solve(idx+word.length(),s,dict,dp)) {
+    //                 return dp[idx]=true;
+    //             }
+    //         }
+    //     }
+
+    //     return dp[idx]=false;
+    // }
+
+
+    boolean solve(int start, String s, Set<String> dict, Boolean[] dp) {
+        if(start == s.length())
             return true;
 
-        if(dp[idx]!=null)
-            return dp[idx];
+        if(dp[start]!=null)
+            return dp[start];
 
-        for(String word: dict) {
-            if(s.startsWith(word,idx)) {
-                if(solve(idx+word.length(),s,dict,dp)) {
-                    return dp[idx]=true;
-                }
-            }
+        for(int end = start+1;end<=s.length();end++) {
+            if(dict.contains(s.substring(start,end)) && solve(end, s, dict, dp))
+                return dp[start] = true;
         }
 
-        return dp[idx]=false;
+        return dp[start] = false;
     }
 }
