ac85717c54926b098e6a85c6921dee772a2b0741
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-08 17:37:44 +0000

    Sync LeetCode submission - Shortest Unsorted Continuous Subarray (java)

---
+++ leetcode-solutions/problems/shortest_unsorted_continuous_subarray/solution.java
@@ -0,0 +1,29 @@
+class Solution { 
+
+    public int findUnsortedSubarray(int[] nums) {
+    int n = nums.length;
+    int left = -1, right = -2; // ensures 0 if already sorted
+    int max = nums[0], min = nums[n - 1];
+
+    for (int i = 1; i < n; i++) {
+        // max = Math.max(max, nums[i]);
+        if (nums[i] < max) {
+            right = i;
+        } else {
+            max = nums[i];
+        }
+    }
+
+    for (int i = n - 2; i >= 0; i--) {
+        // min = Math.min(min, nums[i]);
+        if (nums[i] > min) {
+            left = i;
+        } else {
+            min = nums[i];
+        }
+    }
+
+    return right - left + 1;
+}
+
+}
