efae403f71708bdb6536845e3f50e7fbdd3d1b5c
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-11 02:33:18 +0000

    Sync LeetCode submission - Reverse Linked List II (java)

--- leetcode-solutions/problems/reverse_linked_list_ii/solution.java
+++ leetcode-solutions/problems/reverse_linked_list_ii/solution.java
@@ -10,38 +10,35 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        if(head==null||head.next==null||left==right)
+
+        if(head == null||head.next == null||left == right)
             return head;
 
-        
-        ListNode prev = null, cur = head;
+        ListNode cur = head, prev = null;
         int i=1;
-        while(cur!=null && i!=left) {
+        while(cur!=null && i<left) {
             prev = cur;
             cur = cur.next;
+
             i++;
         }
-
-        ListNode pointerToStart = prev;
+        ListNode start = cur;
+        ListNode prevToStart = prev;
         prev = null;
 
-        ListNode start = cur;
-
-        while(cur!=null && i!=right+1) {
+        //reverse logic
+        while(cur!=null && i<=right) {
             ListNode next = cur.next;
             cur.next = prev;
 
             prev = cur;
             cur = next;
-
             i++;
         }
-
         start.next = cur;
-        if(pointerToStart!=null)
-            pointerToStart.next = prev;
-
-        else 
+        if(prevToStart!=null) {
+            prevToStart.next = prev;
+        } else
             return prev;  //if left is index 0 then it will become a new head
 
         return head;
