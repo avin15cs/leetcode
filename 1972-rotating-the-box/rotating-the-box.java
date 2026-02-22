bb8541c4077ebc1ed2d9d2fcec5390042968d08f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-22 11:18:13 +0530

    Time: 7 ms (89.07%) | Memory: 124.8 MB (83.70%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -0,0 +1,33 @@
+class Solution {
+    public char[][] rotateTheBox(char[][] box) {
+        
+        int r = box.length;
+        int c = box[0].length;
+
+        char[][] boxN= new char[c][r];
+
+        for(int i=0; i<r; i++) {
+            int empty = c-1;
+
+            for(int j = c-1;j>=0;j--) {
+                //If it is rock
+                if(box[i][j]=='*')
+                    empty = j-1;
+
+                else if(box[i][j]=='#') {
+                    box[i][j]='.';
+                    box[i][empty]='#';
+                    empty--;
+                }
+            }
+        }
+
+        for(int i=0; i<r; i++) {
+            for(int j = 0; j<c;j++) {
+                boxN[j][r-i-1]=box[i][j];
+            }
+        }
+
+        return boxN;
+    }
+}
