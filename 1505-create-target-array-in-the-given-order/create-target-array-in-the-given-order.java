043eb070a846345458177776e77ac3c8fd846f7d
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-02-23 06:28:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (7.93%) - LeetSync

--- 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.java
@@ -1,19 +1,33 @@
 class Solution {
-    public int[] createTargetArray(int[] nums, int[] index) {
-        int n = nums.length;
-        int[] target = new int[n];
+    // public int[] createTargetArray(int[] nums, int[] index) {
+    //     int n = nums.length;
+    //     int[] target = new int[n];
 
-        for(int i=0;i<n;i++) {
-            if(index[i]==i)
-                target[i]=nums[i];
+    //     for(int i=0;i<n;i++) {
+    //         if(index[i]==i)
+    //             target[i]=nums[i];
             
-            else {
+    //         else {
 
-                for(int j=i;j>index[i];j--)
-                    target[j]=target[j-1];
+    //             for(int j=i;j>index[i];j--)
+    //                 target[j]=target[j-1];
                 
-                target[index[i]]=nums[i];
-            }
+    //             target[index[i]]=nums[i];
+    //         }
+    //     }
+
+    //     return target;
+    // }
+
+    public int[] createTargetArray(int[] nums, int[] index) {
+        List<Integer> list = new ArrayList<>();
+        int n= nums.length;
+        for(int i=0;i<n;i++)
+            list.add(index[i],nums[i]);
+
+        int target[] = new int[n];
+        for(int i=0;i<n;i++) {
+            target[i] = list.get(i);
         }
 
         return target;
