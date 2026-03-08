3bc9106a5df07a210d56a58ef0867fc59acaf84f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-03-08 22:41:49 +0530

    Time: 1 ms (85.39%) | Memory: 45.5 MB (78.67%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,19 +1,20 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
-        solve(nums,new LinkedHashSet<>(),ans);
+        solve(nums,new ArrayList<>(),ans);
         return ans;
     }
-    public void solve(int[] nums, Set<Integer> path, List<List<Integer>> res) {
+    public void solve(int[] nums, List<Integer> path, List<List<Integer>> res) {
         if(path.size()==nums.length) {
             res.add(new ArrayList<>(path));
             return;
         }
 
         for(int num: nums) {
-            if(path.add(num)){
+            if(!path.contains(num)){
+                path.add(num);
                 solve(nums,path,res);
-                path.remove(num);
+                path.remove(path.size()-1);
             }
         }
     }
