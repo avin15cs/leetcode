6fd395da5728158fc0d0516f7195a6f447ca730c
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-03-12 22:21:23 +0530

    Time: 31 ms (63.95%) | Memory: 96 MB (27.29%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -1,50 +1,50 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        // if(nums.length==0) return 0;
-        // Set<Integer> set=new HashSet<>();
-
-        // for(int num: nums)
-        //     set.add(num);
-
-        // int maxCount=1;
-        // for(int num: nums) {
-        //     if(!set.contains(num-1)){
-        //         int count=1;
-        //         int tnum=num;
-        //         while(set.contains(tnum+1)) {
-        //             count++;
-        //             tnum++;
-        //         }
-        //         maxCount=Math.max(maxCount,count);
-        //     }
-        // }
-        // return maxCount;
-
-        if (nums.length == 0) return 0;
-
         Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
+        for(int num : nums)
             set.add(num);
-        }
 
         int longest = 0;
 
-        for (int num : nums) {
-            int prev=num-1,next=num+1;
-            int currentStreak = 1;
-            while(set.contains(prev)){
-                set.remove(prev--);
-                currentStreak++;
+        for(int num : set){
+            // start only if num is beginning of sequence
+            if(!set.contains(num - 1)){
+                int current = num;
+                int streak = 1;
+                while(set.contains(current + 1)){
+                    current++;
+                    streak++;
+                }
+                longest = Math.max(longest, streak);
             }
-                
-            while (set.contains(next)) {
-                set.remove(next++);
-                currentStreak++;
-            }
-            longest = Math.max(longest, currentStreak);
-            
         }
-
         return longest;
+        
+        // if (nums.length == 0) return 0;
+
+        // Set<Integer> set = new HashSet<>();
+        // for (int num : nums) {
+        //     set.add(num);
+        // }
+
+        // int longest = 0;
+
+        // for (int num : nums) {
+        //     int prev=num-1,next=num+1;
+        //     int currentStreak = 1;
+        //     while(set.contains(prev)){
+        //         set.remove(prev--);
+        //         currentStreak++;
+        //     }
+                
+        //     while (set.contains(next)) {
+        //         set.remove(next++);
+        //         currentStreak++;
+        //     }
+        //     longest = Math.max(longest, currentStreak);
+            
+        // }
+
+        // return longest;
     }
 }
