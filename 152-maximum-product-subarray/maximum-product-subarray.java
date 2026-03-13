dcf6cb23107139c7c8a91db300c29c92653c6718
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-03-13 06:31:50 +0530

    Time: 2 ms (68.29%) | Memory: 47.4 MB (48.74%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,40 +1,21 @@
 class Solution {
-    // public int maxProduct(int[] nums) {
-    //     int max = nums[0];
-    //     int min = nums[0];
-    //     int result = max;
-    //     for(int i=1;i<nums.length;i++) {
-    //         int temp = nums[i];
-    //         int prevMax = max;
-    //         int prevMin = min;
-    //         max = Math.max(temp, Math.max(prevMax*temp,prevMin*temp));
-    //         min = Math.min(temp, Math.min(prevMax*temp,prevMin*temp));
-
-    //         result = Math.max(max, result);
-
-    //     }
-
-    //     return result;
-    // }
     public int maxProduct(int[] nums) {
         int max = nums[0];
-        int p = 1;
-        int n = nums.length;
-        for(int i=0;i<n;i++) {
-            p = p*nums[i];
-            max = Math.max(max,p);
-            if(p==0)
-                p=1;
+        int min = nums[0];
+        int result = nums[0];
+
+        for(int i=1;i<nums.length;i++) {
+            int cur = nums[i];
+            if(cur<0) {
+                int temp = max;
+                max = min;
+                min = temp;
+            }
+            max = Math.max(cur, cur*max);
+            min = Math.min(cur, cur*min);
+            result = Math.max(result, max);
         }
 
-        p=1;
-        for(int i=n-1;i>=0;i--) {
-            p=p*nums[i];
-            max = Math.max(max, p);
-            if(p==0)
-                p=1;
-        }
-
-        return max;
+        return result;
     }
 }
