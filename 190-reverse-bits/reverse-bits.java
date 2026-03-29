1d89bff823c1d31f6a02414e840c4d0f5251f917
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-03-30 00:07:59 +0800

    Time: 1 ms (62.87%) | Memory: 42.6 MB (47.18%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -3,11 +3,12 @@ class Solution {
         int rev=0;
         for(int i=1;i<=32;i++) {
             int mask = n&1;
+
             rev = rev<<1;
 
-            if(mask==1)
-                rev=rev|1;
-
+            if(mask == 1)
+                rev = rev|1;
+            
             n=n>>1;
         }
 
