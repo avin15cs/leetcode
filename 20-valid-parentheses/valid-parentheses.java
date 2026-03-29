a96a4339248bc567c1bb6c400329498129a49c38
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-03-29 23:14:08 +0800

    Time: 3 ms (87.35%) | Memory: 43.2 MB (80.35%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,8 +1,7 @@
 class Solution {
     public boolean isValid(String str) {
         Stack<Character> st = new Stack<>();
-        for(int i=0;i<str.length();i++){
-           
+        for(int i=0;i<str.length();i++) {
             if (str.charAt(i) == '(')
                 st.push(')');
             else if (str.charAt(i) == '{')
@@ -10,10 +9,9 @@ class Solution {
             else if (str.charAt(i) == '[')
                 st.push(']');
             else if (st.empty() || str.charAt(i) != st.pop())
-            {
-                    return false;
-            }
+                return false;
         }
+        
         return st.isEmpty();
     }
 }
