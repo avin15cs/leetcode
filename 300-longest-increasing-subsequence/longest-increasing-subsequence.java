1a598038e9b8aaae0c0e1b21f9de8a356dd8bb3a
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-04-01 09:55:14 +0800

    Time: 6 ms (86.68%) | Memory: 46.3 MB (35.84%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,45 +1,14 @@
 class Solution {
-    // public int lengthOfLIS(int[] nums) {
-    //     List<Integer> list = new ArrayList<>();
-    //     list.add(nums[0]);
-
-    //     for(int i=1;i<nums.length;i++) {
-    //         if(nums[i]>list.get(list.size()-1))
-    //             list.add(nums[i]);
-
-    //         else {
-    //             int pos = findPos(list, nums[i]);
-    //             list.set(pos, nums[i]);
-    //         }       
-    //     }
-    //     return list.size();
-    // }
-
-    // int findPos(List<Integer> list, int target) {
-    //     int i=0, j=list.size()-1;
-    //     while(i<=j) {
-    //         int mid = i+(j-i)/2;
-    //         if(list.get(mid)<target)
-    //             i = mid+1;
-    //         else
-    //             j = mid-1;
-    //     }
-    //     return i; 
-    // }
-
     public int lengthOfLIS(int[] nums) {
-        List<Integer> list=new ArrayList<>();
-
+        List<Integer> list = new ArrayList<>();
         list.add(nums[0]);
 
-        int n=nums.length;
-
-        for(int i=1;i<n;i++) {
-            if(list.get(list.size()-1)<nums[i])
+        for(int i=1;i<nums.length;i++) {
+            if(nums[i]>list.get(list.size()-1))
                 list.add(nums[i]);
-
+            
             else {
-                int pos=findpos(list, nums[i]);
+                int pos = findPos(list, nums[i]);
                 list.set(pos, nums[i]);
             }
         }
@@ -47,11 +16,12 @@ class Solution {
         return list.size();
     }
 
-    int findpos(List<Integer> list, int target) {
+    int findPos(List<Integer> list, int target) {
         int i=0,j=list.size()-1;
 
         while(i<=j) {
             int mid = i+(j-i)/2;
+
             if(list.get(mid)<target)
                 i=mid+1;
             else
@@ -60,4 +30,6 @@ class Solution {
 
         return i;
     }
+
+
 }
