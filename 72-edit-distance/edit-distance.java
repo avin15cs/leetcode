71a83048c39735c5ea86c7ee19b5b8dc2edcfb15
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-04-18 14:05:44 +0800

    Time: 3 ms (98.43%) | Memory: 47.2 MB (38.25%) - LeetSync

---
+++ 72-edit-distance/edit-distance.java
@@ -0,0 +1,99 @@
+class Solution {
+    // public int minDistance(String word1, String word2) {
+    //     int n=word1.length();
+    //     int m=word2.length();
+
+    //     Integer[][] dp =new Integer[n+1][m+1];
+    //     return solve(word1, word2, n, m, dp);
+    // }
+
+
+    // private int solve(String word1, String word2, int n, int m, Integer[][] dp) {
+    //     if(n==0)
+    //         return m;
+
+    //     if(m==0)
+    //         return n;
+
+    //     if(dp[n][m]!=null)
+    //         return dp[n][m];
+        
+    //     if(word1.charAt(n-1)==word2.charAt(m-1))
+    //         return dp[n][m]=solve(word1, word2, n-1, m-1, dp);
+        
+    //     else
+    //         return dp[n][m]=1+Math.min(solve(word1, word2, n-1, m-1, dp), Math.min(solve(word1, word2, n-1, m, dp), solve(word1, word2, n, m-1, dp)));
+
+    // } 
+
+    // public int minDistance(String word1, String word2) {
+    //     int n=word1.length();
+    //     int m=word2.length();
+    //     int[][] dp = new int[n+1][m+1];
+
+    //     for(int i=0;i<=n;i++) {
+    //         for(int j=0;j<=m;j++) {
+    //             if(i==0)
+    //                 dp[0][j]=j;
+    //             else if(j==0)
+    //                 dp[i][0]=i;
+    //             else if(word1.charAt(i-1)==word2.charAt(j-1))
+    //                 dp[i][j]=dp[i-1][j-1];
+    //             else
+    //                 dp[i][j]=1+Math.min(dp[i-1][j-1], Math.min(dp[i-1][j],dp[i][j-1]));
+    //         }
+    //     }
+
+    //     return dp[n][m];
+    // }
+
+    // public int minDistance(String word1, String word2) {
+    //     int n=word1.length();
+    //     int m=word2.length();
+    //     int[] dp = new int[m+1];
+
+    //     for(int i=0;i<=n;i++) {
+    //         int prev=0;//dp[i-1][j-1];
+    //         for(int j=0;j<=m;j++) {
+    //             int temp= dp[j];//dp[i-1][j]
+    //             if(i==0)
+    //                 dp[j]=j;
+    //             else if(j==0)
+    //                 dp[j]=i;
+    //             else if(word1.charAt(i-1)==word2.charAt(j-1))
+    //                 dp[j]=prev;
+    //             else
+    //                 dp[j]=1+Math.min(prev, Math.min(temp,dp[j-1]));
+
+    //             prev = temp;
+    //         }
+    //     }
+    //     return dp[m];
+    // }
+
+    public int minDistance(String word1, String word2) {
+
+        int n = word1.length();
+        int m = word2.length();
+
+        Integer[][] dp = new Integer[n+1][m+1];
+        return solve(word1, word2, n, m, dp);
+    }
+
+    private int solve(String s1, String s2, int n, int m, Integer[][] dp) {
+        if(n==0)
+            return m;
+        
+        if(m==0)
+            return n;
+
+
+        if(dp[n][m]!=null)
+            return dp[n][m];
+
+        if(s1.charAt(n-1)==s2.charAt(m-1))
+            return dp[n][m] = solve(s1,s2,n-1,m-1,dp);
+
+        return dp[n][m] = 1+Math.min(solve(s1,s2,n-1,m-1,dp), Math.min(solve(s1,s2,n,m-1,dp), solve(s1,s2,n-1,m,dp)));
+    } 
+}
