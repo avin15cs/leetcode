62c43d3115d2efd46f22dd7b28b09929ea3a66b2
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-04-27 00:10:37 +0800

    Time: 6 ms (72.68%) | Memory: 44.8 MB (93.62%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,93 @@
+class Solution {
+    // public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    //     List<List<Integer>> res=new ArrayList<>();
+    //     Arrays.sort(candidates);
+    //     solve(candidates,target,candidates.length, res, new ArrayList<>());
+
+    //     return res;
+    // }
+
+    // public void solve(int[] candidates, int target, int n, List<List<Integer>> res, List<Integer> ans)  {
+    //     if(target==0) {
+    //         List<Integer> list = new ArrayList<>(ans);
+    //         res.add(list);
+    //         return;
+    //     }
+    //     if(n<=0||target<=0) {
+    //         return;
+    //     }
+
+    //     if(candidates[n-1]<=target) {
+    //         ans.add(candidates[n-1]);
+    //         solve(candidates,target-candidates[n-1],n-1,res,ans);
+    //         ans.remove(ans.size()-1);
+    //     }
+
+    //     int curpos=n-1;
+    //     int curval=candidates[n-1];
+    //     while(curpos>0 && curval==candidates[curpos-1])
+    //         curpos--;
+
+    //     solve(candidates,target,curpos,res,ans);
+    
+    // }
+
+    // public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    //     List<List<Integer>> res=new ArrayList<>();
+    //     Arrays.sort(candidates);
+    //     backtrack(candidates,0,target,res, new ArrayList<>());
+    //     return res;
+    // }
+
+    // void backtrack(int[] candidates, int start, int target, List<List<Integer>> res, List<Integer> ans) {
+    //     if(target==0) {
+    //         List<Integer> list=new ArrayList<>(ans);
+    //         res.add(list);
+    //         return;
+    //     }
+
+    //     for(int i=start;i<candidates.length;i++) {
+
+    //         // Skip duplicates at the same tree level => ask chatGPT if you dont understand
+    //         if(i>start && candidates[i]==candidates[i-1])
+    //             continue;
+
+    //         if(candidates[i]<=target) {
+    //             ans.add(candidates[i]);
+    //             backtrack(candidates,i+1,target-candidates[i],res,ans);
+    //             ans.remove(ans.size()-1);
+    //         }
+    //     }
+    // }
+
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(candidates, 0, target, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, int target, List<Integer> cur, List<List<Integer>> res) {
+        if(target==0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+
+        if(target<0)
+            return;
+
+        for(int i=start;i<nums.length;i++) {
+            if(nums[i]>target)
+                continue;
+
+            if(i>start && nums[i]==nums[i-1])
+                continue;
+
+            cur.add(nums[i]);
+            backtrack(nums, i+1, target-nums[i],cur, res);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
