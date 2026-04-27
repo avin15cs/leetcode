74bc75a8622871aa01ebd42d0e28025159001f1f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-04-27 23:32:09 +0800

    Time: 134 ms (58.94%) | Memory: 43 MB (61.02%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,59 @@
+class Solution {
+    // public boolean exist(char[][] board, String word) {
+        
+    //     for(int i=0;i<board.length;i++) {
+    //         for(int j=0;j<board[0].length;j++) {
+    //             if(exists(board, word, i, j, 0))
+    //                 return true;
+    //         }
+    //     }
+
+    //     return false;
+    // }
+
+    // boolean exists(char[][] board, String word, int r, int c, int idx) {
+    //     if(idx == word.length())
+    //         return true;
+
+    //     if(r<0||c<0||r==board.length||c==board[0].length||board[r][c]!=word.charAt(idx))
+    //         return false;
+
+    //     board[r][c]='*';
+    //     boolean result=exists(board,word,r+1,c,idx+1)||
+    //                     exists(board,word,r-1,c,idx+1)||
+    //                     exists(board,word,r,c+1,idx+1)||
+    //                     exists(board,word,r,c-1,idx+1);
+    //     board[r][c]=word.charAt(idx);
+    //     return result;
+    // }
+
+    public boolean exist(char[][] board, String word) {
+
+        int n=board.length;
+        int m=board[0].length;
+
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                if(exists(board, word, i, j, 0))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean exists(char[][] board, String word, int i, int j, int idx) {
+        if(idx==word.length())
+            return true;
+
+        if(i<0||j<0||i>=board.length||j>=board[0].length||board[i][j]!=word.charAt(idx)||board[i][j]=='*')
+            return false;
+
+        board[i][j]='*';
+        boolean result = exists(board,word,i+1,j,idx+1)||exists(board,word,i,j+1,idx+1)
+                ||exists(board,word,i-1,j,idx+1)||exists(board,word,i,j-1,idx+1);
+
+        board[i][j]=word.charAt(idx);
+
+        return result;
+    }    
+}
