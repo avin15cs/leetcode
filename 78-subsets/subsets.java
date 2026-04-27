07740658e5dad3b8b496ac2eaacc4f5f9a6a570f
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-04-27 23:07:51 +0800

    Time: 1 ms (88.80%) | Memory: 44 MB (87.66%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,61 @@
+class Solution {
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     int n=nums.length-1;
+    //     return solve(nums,new ArrayList<>(),n);
+    // }
+
+    // List<List<Integer>> solve(int[] nums, List<Integer> cur, int n) {
+    //     if(n<0) {
+    //         List<List<Integer>> ans=new ArrayList<>();
+    //         ans.add(new ArrayList<>(cur));
+    //         return ans;
+    //     }
+    //     List<List<Integer>> left = solve(nums,cur,n-1);
+
+    //     cur.add(nums[n]);
+
+    //     List<List<Integer>> right=solve(nums,cur,n-1);
+    //     left.addAll(right);
+        
+    //     cur.remove(cur.size()-1);
+
+    //     return left;
+    // }
+
+    // public List<List<Integer>> subsets(int[] nums) {
+    //     List<List<Integer>> ans=new ArrayList<>();
+    //     solve(nums,nums.length-1,ans, new ArrayList<>());
+    //     return ans;
+    // }
+
+
+    // private void solve(int[] nums, int n, List<List<Integer>> ans, List<Integer> list) {
+    //     if(n<0) {
+    //         List<Integer> cur=new ArrayList<>(list);
+    //         ans.add(cur);
+    //         return;
+    //     }
+
+    //     solve(nums, n-1, ans, list);
+    //     list.add(nums[n]);
+    //     solve(nums, n-1, ans, list);
+    //     list.remove(list.size()-1);
+    // }
+
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+
+        backtrack(nums, 0, res, new ArrayList<>());
+        return res;
+    }
+
+    private void backtrack(int[] nums, int start, List<List<Integer>> res, List<Integer> cur) {
+        res.add(new ArrayList<>(cur));
+        for(int i=start;i<nums.length;i++) {
+            cur.add(nums[i]);
+            backtrack(nums, i+1, res, cur);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
