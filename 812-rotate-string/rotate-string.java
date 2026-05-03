58d3ce0bae7e57c16fdba7f911a7dc3d7825661a
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-05-03 20:10:50 +0800

    Time: 0 ms (100.00%) | Memory: 41.9 MB (99.86%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,25 +1,37 @@
 class Solution {
-    public boolean rotateString(String s, String goal) {
+    // public boolean rotateString(String s, String goal) {
         
-        if(s.length()!=goal.length())
-            return false;
+    //     if(s.length()!=goal.length())
+    //         return false;
 
-        for(int i=0;i<s.length();i++) {
-            if(s.charAt(i)==goal.charAt(0) && isEqual(s, goal, i))
-                return true;
-        }
+    //     for(int i=0;i<s.length();i++) {
+    //         if(s.charAt(i)==goal.charAt(0) && isEqual(s, goal, i))
+    //             return true;
+    //     }
 
+    //     return false;
+    // }
+
+    // boolean isEqual(String s, String goal, int idx) {
+
+    //     StringBuilder sb = new StringBuilder(s.substring(idx));
+    //     sb.append(s.substring(0,idx));
+
+    //     if(sb.toString().equals(goal))
+    //         return true;
+
+    //     return false;
+    // }   
+
+    public boolean rotateString(String s, String goal) {
+        if(s.length() != goal.length()) return false;
+        
+        s= s+s;
+
+        
+        if(s.indexOf(goal) != -1) return true;
+        
         return false;
+
     }
-
-    boolean isEqual(String s, String goal, int idx) {
-
-        StringBuilder sb = new StringBuilder(s.substring(idx));
-        sb.append(s.substring(0,idx));
-
-        if(sb.toString().equals(goal))
-            return true;
-
-        return false;
-    }   
 }
