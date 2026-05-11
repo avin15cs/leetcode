842e86e494e14bf55180be3279d531b789f28098
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-05-11 10:41:18 +0800

    Time: 0 ms (100.00%) | Memory: 43.1 MB (47.82%) - LeetSync

---
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -0,0 +1,88 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // public ListNode reverseBetween(ListNode head, int left, int right) {
+
+    //     if(head == null||head.next == null||left == right)
+    //         return head;
+
+    //     ListNode cur = head, prev = null;
+    //     int i=1;
+    //     while(cur!=null && i<left) {
+    //         prev = cur;
+    //         cur = cur.next;
+
+    //         i++;
+    //     }
+    //     ListNode start = cur;
+    //     ListNode prevToStart = prev;
+    //     prev = null;
+
+    //     //reverse logic
+    //     while(cur!=null && i<=right) {
+    //         ListNode next = cur.next;
+    //         cur.next = prev;
+
+    //         prev = cur;
+    //         cur = next;
+    //         i++;
+    //     }
+    //     start.next = cur;
+    //     if(prevToStart!=null) {
+    //         prevToStart.next = prev;
+    //     } else
+    //         return prev;  //if left is index 0 then it will become a new head
+
+    //     return head;
+
+    // }
+
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+
+        if(head == null|| head.next == null|| left==right)
+            return head;
+
+        ListNode cur = head, prev = null;
+        int i=1;
+        while(cur!=null && i<left) {
+            prev = cur;
+            cur = cur.next;
+
+            i++;
+        }
+
+        ListNode prevToStart = prev;
+        ListNode start = cur;
+
+        prev = null;
+
+
+        while(cur!=null && i<=right) {
+            ListNode next = cur.next;
+            cur.next = prev;
+
+            prev = cur;
+            cur = next;
+
+            i++;
+        }
+
+        start.next = cur;
+
+        if(prevToStart!=null)
+            prevToStart.next = prev;
+        else
+            return prev;
+
+        return head;
+    }
+}
