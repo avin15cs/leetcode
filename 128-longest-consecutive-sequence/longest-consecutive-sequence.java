8d9837d405bc3e3619e4ea2f5b4fb95a5179cd36
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-05-12 09:09:42 +0800

    Time: 31 ms (65.07%) | Memory: 95.7 MB (45.53%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.java
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.java
@@ -19,32 +19,5 @@ class Solution {
             }
         }
         return longest;
-        
-        // if (nums.length == 0) return 0;
-
-        // Set<Integer> set = new HashSet<>();
-        // for (int num : nums) {
-        //     set.add(num);
-        // }
-
-        // int longest = 0;
-
-        // for (int num : nums) {
-        //     int prev=num-1,next=num+1;
-        //     int currentStreak = 1;
-        //     while(set.contains(prev)){
-        //         set.remove(prev--);
-        //         currentStreak++;
-        //     }
-                
-        //     while (set.contains(next)) {
-        //         set.remove(next++);
-        //         currentStreak++;
-        //     }
-        //     longest = Math.max(longest, currentStreak);
-            
-        // }
-
-        // return longest;
     }
 }
