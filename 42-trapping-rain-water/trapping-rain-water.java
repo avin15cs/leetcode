8ffe177cc44b6c296633933b52c74c0b3710d9de
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-05-26 06:39:52 +0530

    Time: 0 ms (100.00%) | Memory: 47.8 MB (55.48%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,20 +1,52 @@
 class Solution {
+    // public int trap(int[] height) {
+    //     int n = height.length;
+    //     int[] left = new int[n];
+    //     int[] right = new int[n];
+    //     int sum=0; 
+    //     left[0]=height[0];
+    //     right[n-1]=height[n-1];
+    //     for(int i=1;i<n;i++) {
+    //         left[i]=Math.max(left[i-1],height[i]);
+    //         right[n-i-1]=Math.max(right[n-i],height[n-i-1]);
+    //     }
+
+    //     for(int i=0;i<n;i++) {
+    //         sum+=Math.min(left[i],right[i])-height[i];
+    //     }
+
+    //     return sum;
+    // }
+
+
     public int trap(int[] height) {
-        int n = height.length;
-        int[] left = new int[n];
-        int[] right = new int[n];
-        int sum=0; 
-        left[0]=height[0];
-        right[n-1]=height[n-1];
-        for(int i=1;i<n;i++) {
-            left[i]=Math.max(left[i-1],height[i]);
-            right[n-i-1]=Math.max(right[n-i],height[n-i-1]);
-        }
+        int leftMax = 0;
+        int rightMax = 0;
 
-        for(int i=0;i<n;i++) {
-            sum+=Math.min(left[i],right[i])-height[i];
-        }
+        int water = 0;
 
-        return sum;
+        int left = 0, right = height.length-1;
+
+        while(left<=right) {
+            if(height[left]<height[right]) {
+                if(leftMax<height[left])
+                    leftMax = height[left];
+
+                else
+                    water+=leftMax-height[left];
+
+                left++;
+            } else {
+                if(rightMax<height[right])
+                    rightMax = height[right];
+                
+                else
+                    water+=rightMax-height[right];
+
+                right--;
+            }
+        } 
+
+        return water;
     }
 }
