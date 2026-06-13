22c9a483b71612d2314c4a085932eab0c974c1d8
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-06-13 10:38:28 +0530

    Time: 2 ms (88.25%) | Memory: 47.8 MB (61.18%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -20,15 +20,27 @@ class Solution {
 
 
 
-    public boolean canJump(int[] nums)
-    {
+    // public boolean canJump(int[] nums)
+    // {
+    //     int farthest = 0;
+    //     for(int i=0;i<nums.length;i++) {
+    //         if(farthest<i)
+    //             return false;
+    //         farthest = Math.max(nums[i]+i, farthest);
+    //     }
+
+    //     return true;
+    // }
+
+    public boolean canJump(int[] nums) {
         int farthest = 0;
         for(int i=0;i<nums.length;i++) {
             if(farthest<i)
                 return false;
-            farthest = Math.max(nums[i]+i, farthest);
-        }
 
+            farthest = Math.max(farthest, i+nums[i]);
+        }
         return true;
     }
+
 }
