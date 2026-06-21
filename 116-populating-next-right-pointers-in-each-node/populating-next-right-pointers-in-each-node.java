1324ca1fac1aa31bbc4823a50c032e3d253d9064
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-06-21 14:13:42 +0530

    Time: 0 ms (100.00%) | Memory: 46.5 MB (34.11%) - LeetSync

---
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -0,0 +1,47 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        if(root==null)
+            return root;
+
+        Node levelStart = root;
+        //levelStart is for vertically going down to next level from start of each level
+        while(levelStart.left!=null) {
+            Node cur = levelStart;
+            //here we traverse within level => traversal happens with right side movement
+            while(cur!=null) {
+                cur.left.next = cur.right;
+
+                if(cur.next!=null)
+                    cur.right.next = cur.next.left;
+                cur = cur.next;
+            }
+
+            levelStart = levelStart.left;
+        }
+
+        return root;
+    }
+}
