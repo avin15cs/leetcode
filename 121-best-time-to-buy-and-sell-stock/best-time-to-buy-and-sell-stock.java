d86c18d81c05dc1bcc756afe63be082fae93266d
Avinash Modi <39313744+avin15cs@users.noreply.github.com>
2026-08-26 10:24:21 +0530

    Time: 2 ms (44.00%) | Memory: 94.5 MB (51.56%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -12,21 +12,33 @@ class Solution {
     // }
 
 //Kadane's Algo
+    // public int maxProfit(int[] prices) {
+    //     int diff=0, ans=0;
+    //     int cur=0;
+    //     for(int i=1;i<prices.length;i++) {
+    //         diff = prices[i]-prices[i-1];
+
+    //         cur = cur+diff;
+    //         if(cur<0) {
+    //             cur=0;
+    //         }
+
+    //         ans = Math.max(ans, cur);
+    //     }
+
+    //     return ans;
+    // }
+
+
     public int maxProfit(int[] prices) {
-        int diff=0, ans=0;
-        int cur=0;
+        int minPrice = prices[0], maxProfit = 0;
+
         for(int i=1;i<prices.length;i++) {
-            diff = prices[i]-prices[i-1];
-
-            cur = cur+diff;
-            if(cur<0) {
-                cur=0;
-            }
-
-            ans = Math.max(ans, cur);
+            minPrice = Math.min(minPrice, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i]-minPrice);
         }
 
-        return ans;
-    }
+        return maxProfit;
 
+    }
 }
